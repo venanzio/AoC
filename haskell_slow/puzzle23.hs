95434dee00e3e87e71fb724b4adf58c78cd25942
Venanzio Capretta <venanzio@duplavis.com>
2021-01-08 17:29:13 +0000

    day 23: part 1 solved in abstract class

--- haskell_slow/puzzle23.hs
+++ haskell_slow/puzzle23.hs
@@ -27,6 +27,10 @@ class Cycle c where
   mapC     :: Int -> Int -> c -> c  -- inserting/changing a value
   singleC  :: Int -> c   -- singleton cycle
 
+-- move the current element one step clockwise
+rightC :: Cycle c => c -> c
+rightC c = setCurrentC (nextC (currentC c) c) c
+
 -- Printing a cycle, starting with the current element
 showC :: Cycle c => c -> String
 showC c = show $ take (maxC c) $  fromC c (currentC c)
@@ -50,6 +54,92 @@ cups xs mx = setCurrentC x $ mapC z zn $ mapC mx x $ setMaxC mx $ listC xs
   where x = head xs
         z = last xs
         zn = maximum xs + 1
+
+-- Implementation of the moves
+
+-- Taking out the n elements clockwise from the current
+--  The elements are left hanging in the cycle (will be replaced later)
+takeC :: Cycle c => c -> Int -> ([Int],c)
+takeC c n = let x0 = currentC c
+                x = nextC x0 c
+                l = takeL x n c
+                y = nextC (last l) c
+            in (l, mapC x0 y c)
+
+-- take the list of n elements starting from x clockwise
+takeL :: Cycle c => Int -> Int -> c -> [Int]
+takeL x 0 c = []
+takeL x n c = x : takeL (nextC x c) (n-1) c
+
+-- destination: current - 1, avoiding the elements in the list
+destinationC :: Cycle c => [Int] -> c -> Int
+destinationC ys c = dest (currentC c)
+  where mx = maxC c
+        dest x =
+          let x' = if x-1==0 then mx else x-1
+          in if x' `elem` ys then dest x' else x' 
+
+-- insert the elements ys after x in the cycle
+insertC :: Cycle c => Int -> [Int] -> c -> c
+insertC x ys c = insBetween x ys (nextC x c) c
+
+-- insert ys between x and z:
+-- x will point at the first of ys, the last of ys will point at z
+insBetween :: Cycle c => Int -> [Int] -> Int -> c -> c
+insBetween x [] z = mapC x z
+insBetween x (y:ys) z = insBetween y ys z . mapC x y
+
+-- One move: pick up three cups, insert them after the destination,
+--           move the current cup one place clockwise
+moveC :: Cycle c => c -> c
+moveC c = let (picks,c0) = takeC c 3
+              d = destinationC picks c0
+              c1 = insertC d picks c0
+          in rightC c1
+
+-- Performing n moves in sequence
+movesC :: Cycle c => Int -> c -> c
+movesC 0 = id
+movesC n = movesC (n-1) . moveC
+
+-- Part 1
+
+puzzle1 :: String -> String
+puzzle1 s = let c = read s :: MapCycle 
+                c' = movesC 100 c
+            in final c'
+
+final :: MapCycle -> String
+final c = concat $ map show $ (tail $ fromC c 1)
+
+-- Part 2
+
+puzzle2 :: String -> (Int,Int)
+puzzle2 s = let c = cups (readL s) 1000000
+                c' = movesC 10000000 c
+            in resultC c'
+            
+
+
+resultC :: MapCycle -> (Int,Int)
+resultC c = let x1 = nxt c 1
+                x2 = nxt c x1
+            in (x1,x2)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let (x,y) = puzzle2 (head args)
+  putStrLn ("first = " ++ show x ++", second = " ++ show y ++
+            "; product = " ++ show (x*y))
+  return ()
+
+
+
+
+
+
+
 
 
 
@@ -83,88 +173,3 @@ instance Show MapCycle where
 
 instance Read MapCycle where
   readsPrec d src = [(listC (readL src), "")]
-
-
--- Implementation of the moves
-
--- Taking out the n elements clockwise from the current
---  The elements are left hanging in the map (will be replaced later)
-takeC :: MapCycle -> Int -> ([Int],MapCycle)
-takeC c n = let x0 = currentMC c
-                x = nxt c x0
-                l = takeM c x n
-                y = nxt c (last l)
-                m = nextMC c
-            in (l, c { nextMC = M.insert x0 y (nextMC c) })
-
-takeM :: MapCycle -> Int -> Int -> [Int]
-takeM c x 0 = []
-takeM c x n = x : takeM c (nxt c x) (n-1)
-
-destinationC :: MapCycle -> [Int] -> Int
-destinationC c ys = dest (currentMC c)
-  where mx = maxMC c
-        dest x =
-          let x' = if x-1==0 then mx else x-1
-          in if x' `elem` ys then dest x' else x' 
-
--- insert the elements ys after x in the cycle
-insertC :: Int -> [Int] -> MapCycle -> MapCycle
-insertC x ys c = c { nextMC = insBetween x ys (nxt c x) (nextMC c) }
-
--- insert ys between x and z:
--- x will point at the first of ys, last of ys will point at z
-insBetween :: Int -> [Int] -> Int -> M.Map Int Int -> M.Map Int Int
-insBetween x [] z m = M.insert x z m
-insBetween x (y:ys) z m = insBetween y ys z (M.insert x y m)
-
--- One move: pick up three cups, insert them after the destination,
---           move the current cup one place clockwise
-moveC :: MapCycle -> MapCycle
-moveC c = let (picks,c0) = takeC c 3
-              d = destinationC c0 picks
-              c1 = insertC d picks c0
-          in c1 {currentMC = nxt c1 (currentMC c1)} 
-
--- Performing n moves in sequence
-movesC :: MapCycle -> Int -> MapCycle
-movesC c 0 = c
-movesC c n = movesC (moveC c) (n-1)
-
-
-
-
-
--- Part 1
-
-puzzle1 :: String -> String
-puzzle1 s = let w = read s
-                w' = movesC w 100
-            in final w'
-
-final :: MapCycle -> String
-final c = concat $ map show $ (tail $ fromC c 1)
-
--- Part 2
-
-puzzle2 :: String -> (Int,Int)
-puzzle2 s = let c = cups (readL s) 1000000
-                c' = movesC c 10000000
-            in resultC c'
-            
-
-
-resultC :: MapCycle -> (Int,Int)
-resultC c = let x1 = nxt c 1
-                x2 = nxt c x1
-            in (x1,x2)
-
-main :: IO ()
-main = do
-  args <- getArgs
-  let (x,y) = puzzle2 (head args)
-  putStrLn ("first = " ++ show x ++", second = " ++ show y ++
-            "; product = " ++ show (x*y))
-  return ()
-
-
