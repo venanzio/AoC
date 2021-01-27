c319c7e6d19c04bd5c78bcbe98cc9d6d1e7a205c
Venanzio Capretta <venanzio@duplavis.com>
2021-01-27 10:27:57 +0000

    Day 2 python: copied Viper's solution

---
+++ python/day02.py
@@ -0,0 +1,34 @@
+# Advent of Code 2020, Day 2
+
+with open("../input02", "r") as infile:
+     input = infile.readlines()
+
+linp = [i.split("\n")[0] for i in input]
+
+print(linp)
+
+valid_2 = []
+valid_1 = []
+for lin in linp:
+    key, value = lin.split(": ")
+    char = key.split(" ")[1]
+    # print(char, key.split(" ")[0].split("-"))
+    n1, n2 = key.split(" ")[0].split("-")
+    n1, n2 = int(n1)-1, int(n2)-1
+
+    if n1+1<=value.count(char)<=n2+1:
+        valid_1.append(lin)
+
+    c = 0
+    try:
+
+        if value[int(n1)] == char:
+            c+=1
+        if value[int(n2)] == char:
+            c+=1
+        if c == 1:
+            valid_2.append(lin)
+    except:
+        pass
+print(" Solution 1: {len(valid_1)}\nSolution 2:{len(valid_2)}")
+
