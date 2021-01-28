72ecdae18457d764ed10cbb4a4ddf28126b53db3
Venanzio Capretta <venanzio@duplavis.com>
2021-01-28 15:50:37 +0000

    day 3 python

---
+++ python/day03.py
@@ -0,0 +1,29 @@
+# Advent of Code 2020, Day 2
+
+with open("../input03", "r") as infile:
+     input = infile.readlines()
+
+# divide the input into lines
+lines = [i.split("\n")[0] for i in input]
+
+n = len(lines[0])
+m = len(lines)
+
+def countTrees (dx,dy):
+  x = 0
+  y = 0
+  trs = 0
+  while y < m:
+    if lines[y][x] == '#': trs+=1
+    x = (x+dx) % n
+    y = y+dy
+  return(trs)
+
+# Solutions
+
+print('Part 1: ' + str(countTrees(3,1)))
+print('Part 2: ' + str(countTrees (1,1) *
+                       countTrees (3,1) *
+                       countTrees (5,1) *
+                       countTrees (7,1) *
+                       countTrees (1,2)))
