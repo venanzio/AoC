2a2efab401130b13bbb52b5c8e06a023a89fcf40
Venanzio Capretta <venanzio@duplavis.com>
2021-01-29 20:44:59 +0000

    day 4 haskell: simplified the parser

--- haskell_slow/puzzle04.hs
+++ haskell_slow/puzzle04.hs
@@ -18,8 +18,8 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let ps = parseAll passports input
-  putStrLn ("Part 1: " ++ show (countValidPass ps))
-  putStrLn ("Part 2: " ++ show (countValid ps))
+  putStrLn ("Part 1: " ++ show (countValid1 ps))
+  putStrLn ("Part 2: " ++ show (countValid2 ps))
 
 data Passport = Pass 
   { byrP :: Maybe String,
@@ -53,17 +53,17 @@ emptyPass = Pass {
 
 -- part 1
 
-validPass :: Passport -> Bool
-validPass p = byrP p /= Nothing &&
-              iyrP p /= Nothing &&
-              eyrP p /= Nothing &&
-              hgtP p /= Nothing &&
-              hclP p /= Nothing &&
-              eclP p /= Nothing &&
-              pidP p /= Nothing 
+valid1 :: Passport -> Bool
+valid1 p = byrP p /= Nothing &&
+           iyrP p /= Nothing &&
+           eyrP p /= Nothing &&
+           hgtP p /= Nothing &&
+           hclP p /= Nothing &&
+           eclP p /= Nothing &&
+           pidP p /= Nothing 
   
-countValidPass :: [Passport] -> Int
-countValidPass = length . (filter validPass)
+countValid1 :: [Passport] -> Int
+countValid1 = length . (filter valid1)
 
 
 
@@ -74,12 +74,12 @@ maybeParse pa s = case parse pa s of
   [(a,"")] -> Just a
   _        -> Nothing
 
-valid :: Passport -> Bool
-valid p = validByr p && validIyr p && validEyr p && validHgt p &&
-          validHcl p && validEcl p && validPid p
+valid2 :: Passport -> Bool
+valid2 p = validByr p && validIyr p && validEyr p && validHgt p &&
+           validHcl p && validEcl p && validPid p
 
-countValid :: [Passport] -> Int
-countValid = length . (filter valid)
+countValid2 :: [Passport] -> Int
+countValid2 = length . (filter valid2)
 
 validYear :: Int -> Int -> String -> Bool
 validYear low high s = length s == 4 && checkYear (maybeParse natural s)
@@ -145,77 +145,32 @@ passports :: Parser [Passport]
 passports = blocks pass
 
 pass :: Parser Passport
-pass = (byr >>= addByr)
-       <|> (iyr >>= addIyr)
-       <|> (eyr >>= addEyr)
-       <|> (hgt >>= addHgt)
-       <|> (hcl >>= addHcl)
-       <|> (ecl >>= addEcl)
-       <|> (pid >>= addPid)
-       <|> (cid >>= addCid)
-       <|> (space >> return emptyPass)
-  
-byr :: Parser String
-byr = do
-  symbol "byr:"
-  label
+pass = (addField "byr:" chByr) <|>
+       (addField "iyr:" chIyr) <|>
+       (addField "eyr:" chEyr) <|>
+       (addField "hgt:" chHgt) <|>
+       (addField "hcl:" chHcl) <|>
+       (addField "ecl:" chEcl) <|>
+       (addField "pid:" chPid) <|>
+       (addField "cid:" chCid) <|>
+       (space >> return emptyPass)
 
-addByr :: String -> Parser Passport
-addByr b = pass >>= \p -> return $ p {byrP = Just b}
+pField :: String -> Parser String
+pField fname = symbol fname >> label
 
-iyr :: Parser String
-iyr = do
-  symbol "iyr:"
-  label
+addField :: String -> (String -> Passport -> Passport) -> Parser Passport
+addField fname field = do
+  fvalue <- pField fname
+  p <- pass
+  return (field fvalue p)
 
-addIyr :: String -> Parser Passport
-addIyr b = pass >>= \p -> return $ p {iyrP = Just b}
-
-eyr :: Parser String
-eyr = do
-  symbol "eyr:"
-  label
-
-addEyr :: String -> Parser Passport
-addEyr b = pass >>= \p -> return $ p {eyrP = Just b}
-
-hgt :: Parser String
-hgt = do
-  symbol "hgt:"
-  label
-
-addHgt :: String -> Parser Passport
-addHgt b = pass >>= \p -> return $ p {hgtP = Just b}
-
-hcl :: Parser String
-hcl = do
-  symbol "hcl:"
-  label
-
-addHcl :: String -> Parser Passport
-addHcl b = pass >>= \p -> return $ p {hclP = Just b}
-
-ecl :: Parser String
-ecl = do
-  symbol "ecl:"
-  label
-
-addEcl :: String -> Parser Passport
-addEcl b = pass >>= \p -> return $ p {eclP = Just b}
-
-pid :: Parser String
-pid = do
-  symbol "pid:"
-  label
-
-addPid :: String -> Parser Passport
-addPid b = pass >>= \p -> return $ p {pidP = Just b}
-
-cid :: Parser String
-cid = do
-  symbol "cid:"
-  label
-
-addCid :: String -> Parser Passport
-addCid b = pass >>= \p -> return $ p {cidP = Just b}
-
+-- Functions to change passport fields
+-- They all have type String -> Passport -> Passport
+chByr v pass = pass {byrP = Just v}
+chIyr v pass = pass {iyrP = Just v}
+chEyr v pass = pass {eyrP = Just v}
+chHgt v pass = pass {hgtP = Just v}
+chHcl v pass = pass {hclP = Just v}
+chEcl v pass = pass {eclP = Just v}
+chPid v pass = pass {pidP = Just v}
+chCid v pass = pass {cidP = Just v}
