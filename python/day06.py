ee6fe70fd835b08343aca9e046641619801cc3ea
Venanzio Capretta <venanzio@duplavis.com>
2021-02-02 16:11:33 +0000

    day 3 python: Part 1

--- python/day06.py
+++ python/day06.py
@@ -5,28 +5,32 @@ import re
 with open("../input06", "r") as infile:
      lines = infile.readlines()
 
-group = []
-groups = []
-
+# check if a line is blank
 def blank(l):
   b = True
   for c in l: b = b and (c == ' ' or c == '\n')
   return b
 
+group = []
+groups = []
+
 for l in lines:
-  if blank(l):  groups.append(group)
-  else: group.append(l[:-1])
+  if blank(l):  
+    groups.append(group)
+    group = []
+  else: 
+   group.append(l[:-1])
+groups.append(group)
 
 # Part 1
 
 def any_ans(g):
   aa = set()
-  for l in g: aa.union(set(l))
+  for l in g:
+   aa = aa.union(set(l))
   return len(aa)
 
 count = 0
 for g in groups: count += any_ans(g)
-  
 
-
-print(count)
+print("Part 1: " + str(count))
