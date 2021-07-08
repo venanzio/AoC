efde41472e1c87eef2a9bf05778b9d9e0b4d2cec
Venanzio Capretta <venanzio@duplavis.com>
2021-07-08 11:43:44 +0100

    day 8 python: part 1

--- python/day08.py
+++ python/day08.py
@@ -11,6 +11,8 @@ for l in lines:
   (inst,val) = l.split(" ")
   prog.append((inst,int(val)))
 
+# Part 1
+
 # program length
 size = len(prog)
 
@@ -20,9 +22,33 @@ acc = 0
 # next instruction to be executed
 inst = 0
 
+# list of executed instructions
+visited = []
+
+def printstate():
+  print("Accumulator: " + str(acc))
+  print("Instruction: " + str(inst) + " -> " + str(prog[inst]))
+  return ()
+
+def execute((i,x)):
+  global acc
+  global inst
+  if i == 'acc':
+    acc += x
+    inst +=1
+  elif i == 'jmp':
+    inst += x
+  else:
+    inst += 1
+  return ()
+
+while inst not in visited:
+  visited.append(inst)
+  print("visited list: " + str(visited))  
+  execute(prog[inst])
+  printstate()
 
 
 
-
-print(size)
+print(acc)
 
