43bd3bfe7e6b6cb653d11a13d31830f7e88c8ff2
venanzio <venanzio@duplavis.com>
2021-11-27 16:44:43 +0000

    puzzle 9 haskell: comments

--- haskell_slow/puzzle09.hs
+++ haskell_slow/puzzle09.hs
@@ -30,10 +30,13 @@ puzzle fileName = do
 
 -- Part 1
 
+-- pairSum xs y: check if y is the sum of twxo elements of xs
 pairSum :: [Int] -> Int -> Bool
 pairSum [] _ = False
 pairSum (x:xs) y = (y-x) `elem` xs || pairSum xs y
 
+-- check the puzzle property
+-- return the first number that doesn't satisfy it
 noPSum :: [Int] -> [Int] -> Maybe Int
 noPSum xs [] = Nothing
 noPSum xs (y:ys) = if pairSum xs y
@@ -42,11 +45,13 @@ noPSum xs (y:ys) = if pairSum xs y
 
 -- Part 2
 
+-- run xs y: initial segment of xs not smaller than y
 run :: [Int] -> Int -> [Int]
 run (x:xs) y
   | y<=0      = []
   | otherwise = x : run xs (y-x)  
-    
+
+-- find a run of contiguous numbers that add up to y
 findRun :: [Int] -> Int -> Maybe [Int]
 findRun [] y = Nothing
 findRun xs y = let rs = run xs y
