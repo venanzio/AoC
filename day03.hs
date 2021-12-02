ac8fa6b4fa050c83c12d89f0abc6e24d093c0ebc
venanzio <venanzio@duplavis.com>
2021-12-02 05:40:20 +0000

    skeleton for day 3

---
+++ day03.hs
@@ -0,0 +1,39 @@
+-- Advent of Code 2021, day 3
+
+module Main where
+
+import System.Environment
+import Data.List
+import Data.Char
+
+import FunParser
+import Control.Applicative
+
+import qualified Data.Map as M
+
+main :: IO ()
+main = do
+  args <- getArgs
+  puzzle (head args)
+
+puzzle :: String -> IO ()
+puzzle fileName = do
+  input <- readFile fileName
+  let xs = parseAll pInput input
+  putStrLn ("Part 1: " ++ show (part1 xs))
+  putStrLn ("Part 2: " ++ show (part2 xs))
+
+-- Parsing the input
+
+pInput :: Parser ()
+pInput = return ()
+
+-- Part 1
+
+part1 :: () -> Int
+part1 _ = 1
+
+-- Part 2
+
+part2 :: () -> Int
+part2 _ = 2
