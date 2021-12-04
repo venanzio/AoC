6cde4fd608b013f72133c9f1268a3f4ea95f81e7
venanzio <venanzio@duplavis.com>
2021-12-04 17:25:02 +0000

    2020 day 12 slow: part 1

--- 2020/haskell_slow/puzzle12.hs
+++ 2020/haskell_slow/puzzle12.hs
@@ -23,20 +23,52 @@ puzzle fileName = do
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
+-- Data types
+
+type Action = (Char,Int)
+
+type Ship = (Int,Int,Int)
+
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+action :: Parser Char
+action = char 'N' <|> char 'S'  <|> char 'E' <|> char 'W' <|>
+         char 'L'  <|> char 'R'  <|> char 'F'
 
-pInput :: Parser [()]
+pData :: Parser Action
+pData = do act <- action
+           value <- natural
+           return (act,value)
+
+pInput :: Parser [Action]
 pInput = pLines pData
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+move :: Ship -> Action -> Ship
+move (x,y,d) ('N',n) = (x,y+n,d)
+move (x,y,d) ('S',n) = (x,y-n,d)
+move (x,y,d) ('E',n) = (x+n,y,d)
+move (x,y,d) ('W',n) = (x-n,y,d)
+move (x,y,d) ('L',n) = (x,y, (d+n) `mod` 360)
+move (x,y,d) ('R',n) = (x,y, (d-n) `mod` 360)
+move (x,y,d) ('F',n) -- assume the direction is a multiple of 90
+  | d==0                 = (x+n,y,d)
+  | d==90  || d==(-270)  = (x,y+n,d)
+  | d==180 || d ==(-180) = (x-n,y,d)
+  | d==270 || d ==(-90)  = (x,y-n,d)
+  | otherwise            = error ("wrong direction" ++ show d)
+
+navigate :: Ship -> [Action] -> Ship
+navigate = foldl move
+
+mDistance :: Ship -> Int
+mDistance (x,y,_) = abs x + abs y
+
+part1 :: [Action] -> Int
+part1 acts = mDistance $ navigate (0,0,0) acts
 
 -- Part 2
 
-part2 :: [()] -> Int
+part2 :: [Action] -> Int
 part2 _ = 2
