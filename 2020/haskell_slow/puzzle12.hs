ef85f474c9599422a19c88759f5a6c12ce8e8e38
venanzio <venanzio@duplavis.com>
2021-12-05 16:34:59 +0000

    2020 day 2 slow: part 2

--- 2020/haskell_slow/puzzle12.hs
+++ 2020/haskell_slow/puzzle12.hs
@@ -29,6 +29,9 @@ type Action = (Char,Int)
 
 type Ship = (Int,Int,Int)
 
+shipPos :: Ship -> (Int,Int)
+shipPos (x,y,_) = (x,y)
+
 -- Parsing the input
 
 action :: Parser Char
@@ -57,18 +60,44 @@ move (x,y,d) ('F',n) -- assume the direction is a multiple of 90
   | d==90  || d==(-270)  = (x,y+n,d)
   | d==180 || d ==(-180) = (x-n,y,d)
   | d==270 || d ==(-90)  = (x,y-n,d)
-  | otherwise            = error ("wrong direction" ++ show d)
+  | otherwise            = error ("wrong direction: " ++ show d)
 
 navigate :: Ship -> [Action] -> Ship
 navigate = foldl move
 
-mDistance :: Ship -> Int
-mDistance (x,y,_) = abs x + abs y
+mDistance :: (Int,Int) -> Int
+mDistance (x,y) = abs x + abs y
 
 part1 :: [Action] -> Int
-part1 acts = mDistance $ navigate (0,0,0) acts
+part1 acts = mDistance $ shipPos $ navigate (0,0,0) acts
 
 -- Part 2
 
+type ShipW = ((Int,Int),(Int,Int))  -- position of ship + waypoint
+
+-- moving the waypoint
+moveW :: (Int,Int) -> Action -> (Int,Int)
+moveW (x,y) ('N',n) = (x,y+n)
+moveW (x,y) ('S',n) = (x,y-n)
+moveW (x,y) ('E',n) = (x+n,y)
+moveW (x,y) ('W',n) = (x-n,y)
+moveW (x,y) ('L',n)
+  | n==0                 = (x,y)
+  | n==90 || n==(-270)   = (-y,x)
+  | n==180 || n ==(-180) = (-x,-y)
+  | n==270 || n ==(-90)  = (y,-x)
+  | otherwise            = error ("wrong direction" ++ show n)
+moveW (x,y) ('R',n) = moveW (x,y) ('L',-n)
+moveW (x,y) ('F',n) = error "Can't do an F on the waypoint"
+
+-- moving the ship
+moveSW  :: ShipW -> Action -> ShipW
+moveSW ((x,y),w@(dx,dy)) ('F',n) = ((x+n*dx,y+n*dy),w)
+moveSW (s,w) a = (s,moveW w a)
+
+navigateW :: ShipW -> [Action] -> ShipW
+navigateW = foldl moveSW
+
 part2 :: [Action] -> Int
-part2 _ = 2
+part2 acts = mDistance $ fst $ navigateW ((0,0),(10,1)) acts
+
