2832e73ae1762fbb5a8a26300a0c52b10736435c
venanzio <venanzio@duplavis.com>
2021-12-05 06:04:22 +0000

    solutions day 5

--- day05.hs
+++ day05.hs
@@ -25,18 +25,71 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+type Coords = (Int,Int)
 
-pInput :: Parser [()]
+type Floor = M.Map Coords Int
+
+point :: Floor -> Coords -> Int
+point f c = f M.! c
+
+pData :: Parser (Coords,Coords)
+pData = do x0 <- natural
+           symbol ","
+           y0 <- natural
+           symbol "->"
+           x1 <- natural
+           symbol ","
+           y1 <- natural
+           return ((x0,y0),(x1,y1))
+           
+pInput :: Parser [(Coords,Coords)]
 pInput = pLines pData
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+initFloor :: Floor
+initFloor = M.empty
+
+range :: Int -> Int -> [Int]
+range n m = if n<=m then [n..m] else [n,n-1..m]
+
+ovLine :: Coords -> Coords -> [Coords]
+ovLine (x0,y0) (x1,y1)
+  | x0==x1 = [(x0,y) | y <- range y0 y1]
+  | y0==y1 = [(x,y0) | x <- range x0 x1]
+  | otherwise = []
+
+mark :: Floor -> Coords -> Floor
+mark f c = case M.lookup c f of
+  Nothing -> M.insert c 1 f
+  Just n  -> M.insert c (n+1) f
+
+markLine :: Floor -> (Coords,Coords) -> Floor
+markLine f (c0,c1) = foldl mark f (ovLine c0 c1)
+
+markAll :: Floor -> [(Coords,Coords)] -> Floor
+markAll = foldl markLine
+
+countOverlap :: Floor -> Int
+countOverlap = length . filter (>=2) . M.elems
+
+part1 :: [(Coords,Coords)] -> Int
+part1 coords = countOverlap (markAll initFloor coords)
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+ventLine :: Coords -> Coords -> [Coords]
+ventLine (x0,y0) (x1,y1)
+  | x0==x1 = [(x0,y) | y <- range y0 y1]
+  | y0==y1 = [(x,y0) | x <- range x0 x1]
+  | otherwise = zip (range x0 x1) (range y0 y1)
+
+
+markLine' :: Floor -> (Coords,Coords) -> Floor
+markLine' f (c0,c1) = foldl mark f (ventLine c0 c1)
+
+markAll' :: Floor -> [(Coords,Coords)] -> Floor
+markAll' = foldl markLine'
+
+part2 :: [(Coords,Coords)] -> Int
+part2 coords = countOverlap (markAll' initFloor coords)
