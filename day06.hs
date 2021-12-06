14a6f0b37be5416dcef9b528f72e64b5e2f1650c
venanzio <venanzio@duplavis.com>
2021-12-06 05:24:18 +0000

    solutions day 6

--- day06.hs
+++ day06.hs
@@ -25,18 +25,36 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
-
-pInput :: Parser [()]
-pInput = pLines pData
+pInput :: Parser [Int]
+pInput = seqSep natural ","
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+nextDay :: [Int] -> [Int]
+nextDay [] = []
+nextDay (x:xs) = if x == 0 then 6:8:nextDay xs
+                           else (x-1):nextDay xs
+
+nextN :: [Int] -> Int -> [Int]
+nextN xs 0 = xs
+nextN xs n = nextN (nextDay xs) (n-1)
+
+part1 :: [Int] -> Int
+part1 xs = length (nextN xs 80)
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+nDay :: [Int] -> [Int]
+nDay ls = [if i == 6 then ls!!0 + ls!!7
+            else if i == 8 then ls!!0 else ls!!(i+1) | i <- [0..8]]
+
+next :: [Int] -> Int -> [Int]
+next ls 0 = ls
+next ls n = next (nDay ls) (n-1)
+
+start :: [Int] -> [Int]
+start xs = [count i | i <- [0..8]]
+  where count i = length (filter (==i) xs)
+
+part2 :: [Int] -> Int
+part2 xs = sum (next (start xs) 256)
