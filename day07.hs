1f5853ac020cd2d67c7b1d60161d91ead413f2bf
venanzio <venanzio@duplavis.com>
2021-12-07 05:45:41 +0000

    day 7: generalized and improved code

--- day07.hs
+++ day07.hs
@@ -33,21 +33,23 @@ pInput = seqSep natural ","
 
 -- Part 1
 
+dist1 :: Int -> Int -> Int
+dist1 y x = abs (y-x)
 
-dist :: [Int] -> Int -> Int
-dist xs y = sum (map (abs.(y-)) xs)
+totDist :: (Int -> Int -> Int) -> [Int] -> Int -> Int
+totDist dist xs y = sum (map (dist y) xs)
+
+minDist :: (Int -> Int -> Int) -> [Int] -> Int
+minDist dist xs = minimum [totDist dist xs y | y <- [0..(maximum xs)]]
 
 part1 :: [Int] -> Int
-part1 xs = minimum [dist xs y | y <- [0..(maximum xs)]]
+part1 =  minDist dist1
 
 -- Part 2
 
-dst :: Int -> Int -> Int
-dst y x = let d = abs (y-x) in d * (d+1) `div` 2
-
-dist2 :: [Int] -> Int -> Int
-dist2 xs y = sum (map (dst y) xs)
+dist2 :: Int -> Int -> Int
+dist2 y x = let d = dist1 y x in d * (d+1) `div` 2
 
 part2 :: [Int] -> Int
-part2 xs = minimum [dist2 xs y | y <- [0..(maximum xs)]]
+part2 = minDist dist2
 
