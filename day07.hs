c5cecf4bae2ff67b6062d693f31d566ea89f5845
venanzio <venanzio@duplavis.com>
2021-12-07 05:30:45 +0000

    solutions day 7

--- day07.hs
+++ day07.hs
@@ -28,15 +28,26 @@ puzzle fileName = do
 pData :: Parser ()
 pData = return ()
 
-pInput :: Parser [()]
-pInput = pLines pData
+pInput :: Parser [Int]
+pInput = seqSep natural ","
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+
+dist :: [Int] -> Int -> Int
+dist xs y = sum (map (abs.(y-)) xs)
+
+part1 :: [Int] -> Int
+part1 xs = minimum [dist xs y | y <- [0..(maximum xs)]]
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+dst :: Int -> Int -> Int
+dst y x = let d = abs (y-x) in d * (d+1) `div` 2
+
+dist2 :: [Int] -> Int -> Int
+dist2 xs y = sum (map (dst y) xs)
+
+part2 :: [Int] -> Int
+part2 xs = minimum [dist2 xs y | y <- [0..(maximum xs)]]
+
