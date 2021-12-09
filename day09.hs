c540e184a59209ef96f475bf4d695a87afd4d6b1
venanzio <venanzio@duplavis.com>
2021-12-09 06:13:00 +0000

    solutions day 9

--- day09.hs
+++ day09.hs
@@ -26,18 +26,56 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+pData :: Parser [Int]
+pData = some (digit >>= \d -> return (read [d]))
 
-pInput :: Parser [()]
+pInput :: Parser [[Int]]
 pInput = pLines pData
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+near :: (Int,Int) -> [(Int,Int)]
+near (i,j) = [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]
+
+adjacent :: [[Int]] -> (Int,Int) -> [Int]
+adjacent xs (i,j) = [xs!!i'!!j' |
+                     (i',j') <- near (i,j),
+                     i'>=0, j'>=0, i' < length xs, j' < length (head xs)]
+
+lowPoint ::  Int -> [Int] -> Bool
+lowPoint x ys = all (x<) ys
+
+lowPoints :: [[Int]] -> [Int]
+lowPoints xs = [xs!!i!!j | i <- [0..length xs -1], j<- [0..length (head xs) -1],
+                           lowPoint (xs!!i!!j) (adjacent xs (i,j))]
+
+part1 :: [[Int]] -> Int
+part1 xs = sum $ map (+1) $ lowPoints xs
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+adj :: [[Int]] -> (Int,Int) -> [(Int,Int)]
+adj xs (i,j) = [(i',j') |
+                (i',j') <- near (i,j),
+                i'>=0, j'>=0, i' < length xs, j' < length (head xs)]
+
+basin :: [[Int]] -> (Int,Int) -> [(Int,Int)]
+basin xs (i,j) = basCum [] [(i,j)]
+  where basCum as [] = as
+        basCum as bs =
+          let as' = as++bs
+              cs = nub [(i',j') | (i,j) <- bs, (i',j') <- adj xs (i,j),
+                        xs!!i'!!j' >= xs!!i!!j, xs!!i'!!j' < 9] \\ as'
+          in basCum as' cs
+
+lPoints :: [[Int]] -> [(Int,Int)]
+lPoints xs = [(i,j) | i <- [0..length xs -1], j<- [0..length (head xs) -1],
+                      lowPoint (xs!!i!!j) (adjacent xs (i,j))]
+
+
+
+basins :: [[Int]] -> [[(Int,Int)]]
+basins xs = map (basin xs) (lPoints xs)
+
+part2 :: [[Int]] -> Int
+part2 xs = product $ take 3 $ reverse $ sort $ map length (basins xs)
