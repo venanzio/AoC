b8ff9b0e5ca5f9b31b5cbdd88959612beebdec25
venanzio <venanzio@duplavis.com>
2021-12-10 07:57:01 +0000

    day 10 part 1 better solution

--- day10.hs
+++ day10.hs
@@ -34,33 +34,29 @@ pInput = pLines (some item)
 
 -- Part 1
 
-data Error = Complete | Incomplete | Corrupted Char
+data Error = Complete | Incomplete String | Corrupted Char
   deriving (Eq,Show)
 
 matches = [('(',')'),('[',']'),('{','}'),('<','>')]
 
+match c = let Just c' = lookup c matches in c'
+
 ops = map fst matches
 cls = map snd matches
 
-chunkE :: Parser Error
-chunkE = do c <- choice (map char ops)
-            e <- chunksE
-            case e of
-              Complete -> do c' <- choice (map char cls)
-                             if (c,c') `elem` matches then return Complete else return (Corrupted c')
-                          <|> return Incomplete
-              e' -> return e'
 
-chunksE :: Parser Error
-chunksE = do e <- chunkE
-             case e of
-               Complete -> chunksE
-               e -> return e
-          <|> return Complete
-
+parMatch :: String -> String -> Error
+parMatch [] [] = Complete
+parMatch [] stack = Incomplete stack
+parMatch (c:cs) stack
+  | c `elem` ops = parMatch cs (match c : stack)
+parMatch (c:cs) (c':stack)
+  | c `elem` cls = if c == c' then parMatch cs stack
+                              else Corrupted c
+parMatch _ _ = error "empty stack"
 
 score :: String -> Int
-score s = let ((e,_):_) = parse chunksE s in case e of
+score cs = case parMatch cs [] of
   Corrupted ')' -> 3
   Corrupted ']' -> 57
   Corrupted '}' -> 1197
@@ -70,7 +66,20 @@ score s = let ((e,_):_) = parse chunksE s in case e of
 part1 :: [String] -> Int
 part1 = sum . map score
 
+
 -- Part 2
 
+scoreC :: String -> Int
+scoreC = foldl (\y x -> 5*y + scr x ) 0
+  where scr ')' = 1
+        scr ']' = 2
+        scr '}' = 3
+        scr '>' = 4
+
+scoreLine :: String -> Maybe Int
+scoreLine xs = case parMatch xs [] of
+  Incomplete stack -> Just (scoreC stack)
+  _ -> Nothing
+
 part2 :: [String] -> Int
-part2 _ = 2
+part2 cs = undefined
