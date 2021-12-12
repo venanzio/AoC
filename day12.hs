87b1479bef31fd02fb1f88aa31ebeaddbd02313f
venanzio <venanzio@duplavis.com>
2021-12-12 06:26:18 +0000

    day 12, part 1

--- day12.hs
+++ day12.hs
@@ -1,4 +1,4 @@
--- Advent of Code 2021, day ?
+-- Advent of Code 2021, day 12
 
 module Main where
 
@@ -21,23 +21,55 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
+  putStrLn (show $ head $ paths xs)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
-
-pInput :: Parser [()]
-pInput = pLines pData
+pData :: Parser (String,String)
+pData = do s <- token (many alphanum)
+           string "-"
+           t <- token (many alphanum)
+           return (s,t)
+           
+pInput :: Parser [(String,String)]
+pInput = some pData
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+caves :: [(String,String)] -> [String]
+caves = nub . foldl (\cs (s,t) -> s:t:cs) []
+
+bigCaves :: [String] -> [String]
+bigCaves = filter (isUpper.head)
+
+small :: String -> Bool
+small cave = isLower (head cave)
+
+smallCaves :: [String] -> [String]
+smallCaves = filter (isLower.head)
+
+edges :: [(String,String)] -> [(String,String)]
+edges xs = [(s,t) | (s,u) <- xs, isLower (head s), (u',t) <- xs, u==u']
+
+
+connected :: [(String,String)] -> String -> [String]
+connected edges s = map snd (filter ((==s).fst) edges) ++ map fst (filter ((==s).snd) edges)
+
+pathNext :: [(String,String)] -> [String] -> [[String]]
+pathNext edges p = [p++[t] | t <- connected edges (last p), not (small t && t `elem` p)]
+
+pathExtend :: [(String,String)] -> [String] -> [[String]]
+pathExtend edges p = [p] ++ (pathNext edges p >>= pathExtend edges)
+
+paths :: [(String,String)] -> [[String]]
+paths edges = pathExtend edges ["start"]
+
+part1 :: [(String,String)] -> Int
+part1 edges = length (filter ((=="end").last) (paths edges))
 
 -- Part 2
 
-part2 :: [()] -> Int
+part2 :: [(String,String)] -> Int
 part2 _ = 2
