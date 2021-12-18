95642e0ec9fcd5817087ab300a6870e68cdde391
venanzio <venanzio@duplavis.com>
2021-12-18 13:21:33 +0000

    day 18 solution part 1

--- day18.hs
+++ day18.hs
@@ -21,7 +21,7 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
-  putStrLn (show $ foldl1 addSN xs)
+  -- putStrLn (show $ foldl1 addSN xs)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
@@ -30,7 +30,12 @@ puzzle fileName = do
 data SNum = Reg Int | Pair SNum SNum
   deriving (Eq,Show)
 
+showSN :: SNum -> String
+showSN (Reg x) = show x
+showSN (Pair x y) = "[" ++ showSN x ++ "," ++ showSN y ++ "]"
+
 data Action = Explode (Maybe Int) (Maybe Int) | Split | NoA
+  deriving (Eq,Show)
 
 pSNum :: Parser SNum
 pSNum = (natural >>= return.Reg) <|>
@@ -47,27 +52,43 @@ pInput = pLines pSNum
 -- Part 1
 
 reduce :: SNum -> SNum
-reduce x = let (a,x') = reduceDepth 0 x in
+reduce x = let (a,x') = reduceStep 0 x in
   case a of
     NoA -> x'
     _   -> reduce x'
-  
-reduceDepth :: Int -> SNum -> (Action,SNum)
-reduceDepth n (Reg x) | x>=10 = (Split, split x)
-reduceDepth 4 (Pair (Reg x) (Reg y)) = (Explode (Just x) (Just y), (Reg 0))
-reduceDepth n (Pair x y) =
-  let (ax,x') = reduceDepth (n+1) x in
+
+redExplode :: Int -> SNum -> (Action,SNum)
+redExplode 4 (Pair (Reg x) (Reg y)) = (Explode (Just x) (Just y), (Reg 0))
+redExplode n (Pair x y) =
+  let (ax,x') = redExplode (n+1) x in
     case ax of
       Explode u (Just v) -> (Explode u Nothing, Pair x' (addL v y))
       Explode u Nothing -> (Explode u Nothing, Pair x' y)
-      Split -> (Split, Pair x' y)
-      NoA -> let (ay,y') = reduceDepth (n+1) y in
+      NoA -> let (ay,y') = redExplode (n+1) y in
                case ay of
                  Explode (Just u) v -> (Explode Nothing v, Pair (addR u x') y')
                  Explode Nothing v -> (Explode Nothing v, Pair x' y')
+                 NoA -> (NoA, Pair x' y')
+redExplode n x = (NoA,x)
+
+redSplit :: Int -> SNum -> (Action,SNum)
+redSplit n (Reg x) | x>=10 = (Split, split x)
+redSplit n (Pair x y) =
+  let (ax,x') = redSplit (n+1) x in
+    case ax of
+      Split -> (Split, Pair x' y)
+      NoA -> let (ay,y') = redSplit (n+1) y in
+               case ay of
                  Split -> (Split, Pair x' y')
                  NoA -> (NoA, Pair x' y')
-reduceDepth n x = (NoA,x)
+redSplit n x = (NoA,x)
+
+reduceStep :: Int -> SNum -> (Action,SNum)
+reduceStep n x =
+  let (a,x') = redExplode n x in
+    case a of
+      Explode _ _ -> (a,x')
+      NoA -> redSplit n x
 
 addL :: Int -> SNum -> SNum
 addL v (Reg x) = Reg (v+x)
