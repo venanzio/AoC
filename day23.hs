eaf6d84f26f3f32c8fb49e84848eb8b8fbe44af5
venanzio <venanzio@duplavis.com>
2021-12-23 06:06:19 +0000

    day 23 parsing the input

--- day23.hs
+++ day23.hs
@@ -21,23 +21,38 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
+  putStrLn (show xs)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
 -- Parsing the input
+data Antiphod = A | B | C | D | E
+  deriving (Eq,Show)
+type Burrow = [[Antiphod]]
 
-pData :: Parser ()
-pData = return ()
+pSkip :: Parser ()
+pSkip = many (sat (\c -> c`elem` ".#" || isSpace c)) >> return ()
 
-pInput :: Parser [()]
-pInput = pLines pData
+pAntiphod :: Parser Antiphod
+pAntiphod = do pSkip
+               (char 'A' >> return A) <|>
+                 (char 'B' >> return B) <|>
+                   (char 'C' >> return C) <|>
+                     (char 'D' >> return D)
+
+pRooms :: [[Antiphod]] -> Parser [[Antiphod]]
+pRooms [r1,r2,r3,r4] =
+  (pAntiphod >>= \a -> pRooms [r2,r3,r4,r1++[a]]) <|> (pSkip >> return [r1,r2,r3,r4])
+
+pInput :: Parser Burrow
+pInput = pRooms  [[],[],[],[]] >>= return . (take 11 (repeat E) :)
 
 -- Part 1
 
-part1 :: [()] -> Int
+part1 :: Burrow -> Int
 part1 _ = 1
 
 -- Part 2
 
-part2 :: [()] -> Int
+part2 :: Burrow -> Int
 part2 _ = 2
