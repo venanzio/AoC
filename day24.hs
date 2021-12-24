effad3e06192500eca492a2a8221cf90ec7953c1
venanzio <venanzio@duplavis.com>
2021-12-24 08:47:41 +0000

    day24 parsing of input

--- day24.hs
+++ day24.hs
@@ -21,23 +21,56 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
+  putStrLn (show xs)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+data Var = W | X | Y | Z
+  deriving (Show,Eq)
+data Instruction = Inp Var
+                 |  Add Var (Either Var Int)
+                 |  Mul Var (Either Var Int)
+                 |  Div Var (Either Var Int)
+                 |  Mod Var (Either Var Int)
+                 |  Eql Var (Either Var Int)
+  deriving (Show,Eq)
 
-pInput :: Parser [()]
-pInput = pLines pData
+pVar :: Parser Var
+pVar = (symbol "w" >> return W) <|>
+       (symbol "x" >> return X) <|>
+       (symbol "y" >> return Y) <|>
+       (symbol "z" >> return Z)
+
+pArg :: Parser (Either Var Int)
+pArg = (pVar >>= return . Left) <|> (integer >>= return . Right)
+
+pBin :: String -> (Var -> Either Var Int -> Instruction) -> Parser Instruction
+pBin name const =
+  do symbol name
+     a <- pVar
+     b <- pArg
+     return (const a b)
+
+pInst :: Parser Instruction
+pInst =
+  (symbol "inp" >> pVar >>= return . Inp) <|>
+  (pBin "add" Add) <|>
+  (pBin "mul" Mul) <|>
+  (pBin "div" Div) <|>
+  (pBin "mod" Mod) <|>
+  (pBin "eql" Eql)
+  
+pInput :: Parser [Instruction]
+pInput = pLines pInst
 
 -- Part 1
 
-part1 :: [()] -> Int
+part1 :: [Instruction] -> Int
 part1 _ = 1
 
 -- Part 2
 
-part2 :: [()] -> Int
+part2 :: [Instruction] -> Int
 part2 _ = 2
