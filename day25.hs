f32c1902f7b224cb5ed06964187eff846683e999
venanzio <venanzio@duplavis.com>
2021-12-25 14:16:30 +0000

    day 24 : movement right

--- day25.hs
+++ day25.hs
@@ -25,6 +25,17 @@ puzzle fileName = do
   -- putStrLn ("Part 1: " ++ show (part1 xs))
   -- putStrLn ("Part 2: " ++ show (part2 xs))
 
+readF :: IO Floor
+readF = readFile "exinput" >>= return . parseAll pInput
+
+showF :: Floor -> String
+showF floor = sF 0 0 where
+  sF x y = case M.lookup (x,y) floor of
+    Just c -> c : sF (x+1) y
+    Nothing -> '\n' : if M.lookup (0,y+1) floor == Nothing
+                      then ""
+                      else sF 0 (y+1)                    
+
 -- Parsing the input
 
 type Floor = M.Map (Int,Int) Char
@@ -39,8 +50,24 @@ pInput = pLines pRow >>= return . mMap
 
 nextR :: Floor -> (Int,Int) -> (Int,Int)
 nextR floor (x,y) = case M.lookup (x,y) floor of
-  Just '>' -> case M.lookup (x+1,y) of
-           Just ..
+  Just '>' -> case M.lookup (x+1,y) floor of
+           Nothing -> case M.lookup (0,y) floor of
+             Just '.' -> (0,y)
+             _        -> (x,y)
+           Just '.' -> (x+1,y)
+           _        -> (x,y)
+  _        -> (x,y)
+
+mobileR :: Floor -> [((Int,Int),(Int,Int))]
+mobileR floor = filter (\((x,y),(newx,newy)) -> x/=newx || y/=newy)
+                       (map (\p -> (p,nextR floor p)) (M.keys floor))
+
+moveR :: Floor -> Maybe Floor
+moveR floor = case mobileR floor of
+  [] -> Nothing
+  moves -> Just $
+           foldl (\f ((x,y),(newx,newy)) -> M.insert (newx,newy) '>' $ M.insert (x,y) '.' f)
+                 floor moves
 
 part1 :: Floor -> Int
 part1 _ = 1
