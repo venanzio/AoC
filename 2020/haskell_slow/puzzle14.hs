b2afd2177e93f4d8792915088a2bd7f2f20945f6
venanzio <venanzio@duplavis.com>
2022-01-10 14:51:01 +0000

    2020 day 14: revision of part 1

--- 2020/haskell_slow/puzzle14.hs
+++ 2020/haskell_slow/puzzle14.hs
@@ -21,7 +21,7 @@ puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
   putStrLn ("Part 1: " ++ show (part1 xs))
-  putStrLn ("Part 2: " ++ show (part2 xs))
+--  putStrLn ("Part 2: " ++ show (part2 xs))
 
 -- Parsing the input
 
@@ -58,6 +58,32 @@ data Mem = Mem { mask :: String
                }
   deriving (Eq,Show)
 
+type Binary = [Int]
+
+decBin :: Int -> Binary
+decBin n = dbAcc n [] where
+  dbAcc 0 ds = ds
+  dbAcc n ds = dbAcc (n `div` 2) (n `mod` 2:ds)
+
+-- binary representation with 36 bits
+bin36 :: Int -> Binary
+bin36 n = let ds = decBin n in (take (36 - length ds) (repeat 0)) ++ ds
+
+binDec :: Binary -> Int
+binDec = foldl (\x d -> 2*x + d) 0
+
+-- assume that the mask and the binary numbers have the same number of bits
+applyMask :: String -> Binary -> Binary
+applyMask "" [] = []
+applyMask ('0':m) (d:x) = 0 : applyMask m x
+applyMask ('1':m) (d:x) = 1 : applyMask m x
+applyMask ('X':m) (d:x) = d : applyMask m x
+applyMask _ _ = error "mask character doesn't match bit"
+
+appMask :: String -> Int -> Int
+appMask m = binDec . applyMask m . bin36
+
+{-
 binRep :: Int -> String
 binRep x = reverse $ bR x 36
   where bR x 0 = ""
@@ -80,6 +106,7 @@ applyMask _ _ = error "wrong character in mask"
 appMask :: String -> Int -> Int
 appMask m x = binNum $ applyMask m (binRep x)
 
+-}
 
 exec :: Mem -> Inst -> Mem
 exec mem (UpdateMask m) = mem {mask = m}
@@ -101,6 +128,7 @@ part1 prog = memValue (execute prog)
 
 -- Part 2
 
+{-
 maskAddress :: String -> String -> [String]
 maskAddress "" a = [a]
 maskAddress ('0':m) (d:a) = map (d:) (maskAddress m a)
@@ -124,3 +152,4 @@ execute2 = foldl exec2 initMem
 
 part2 :: [Inst] -> Int
 part2 prog = memValue (execute2 prog)
+-}
