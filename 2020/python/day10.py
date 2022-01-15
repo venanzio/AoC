fe504366d79ff71a0d80df8ba55349da3295dc3d
venanzio <venanzio@duplavis.com>
2022-01-15 15:01:45 +0000

    2020 day 10 python: skeleton

---
+++ 2020/python/day10.py
@@ -0,0 +1,20 @@
+# Advent of Code 2020, Day 10
+
+with open("../input", "r") as infile:
+     lines = infile.readlines()
+
+# Parsing the input
+
+# ....
+
+
+# Part 1
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
