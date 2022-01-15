270b58804c592c8918c86138963c175464de4d79
venanzio <venanzio@duplavis.com>
2022-01-15 15:58:41 +0000

    day 10 python: part 1

--- 2020/python/day10.py
+++ 2020/python/day10.py
@@ -4,7 +4,7 @@
 
 input = open("../input")
 
-adapters = []
+adapters = [0]
 for l in input.readlines():
   adapters.append(int(l))
 
@@ -15,9 +15,15 @@ input.close()
 adapters.sort()
 adapters.append(adapters[-1]+3)
 
-print(adapters)
+ones = 0
+threes = 0
 
-print("Part 1: ")
+for i in range(1,len(adapters)):
+  diff = adapters[i]-adapters[i-1]
+  if diff==1: ones+=1
+  elif diff==3: threes+=1
+
+print("Part 1: " + str(ones*threes))
 
 
 # Part 2
