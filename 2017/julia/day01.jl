af664e0114141a6e4f18bfc16e6428b22625b453
venanzio <venanzio@duplavis.com>
2022-01-19 11:08:38 +0000

    day 1, part 1 problem was to ignore the newline character at the end of the string

--- 2017/julia/day01.jl
+++ 2017/julia/day01.jl
@@ -4,13 +4,15 @@ f = open("../input")
 input = read(f,String)
 close(f)
 
+lastIndx = lastindex(input)-1 # ignore ending newline
+
 s = 0
 
-for i = firstindex(input):lastindex(input)
-  j = i==lastindex(input) ? 1 : i+1
+for i = firstindex(input):lastIndx
+  j = i==lastIndx ? 1 : i+1
   if input[i] == input[j]
     global s += parse(Int,input[i])
   end
 end
 
-println(s)
+println("part 1: ",s)
