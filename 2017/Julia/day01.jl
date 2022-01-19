f344cf94b93b8614e2987dc7fb4083572257e088
venanzio <venanzio@duplavis.com>
2022-01-19 10:23:52 +0000

    day1: solution to part 1 (incorrect)

--- 2017/Julia/day01.jl
+++ 2017/Julia/day01.jl
@@ -1,1 +1,16 @@
 # Advent of Code 2017 - day 1
+
+f = open("../input")
+input = read(f,String)
+close(f)
+
+s = 0
+
+for i = firstindex(input):lastindex(input)
+  j = i==lastindex(input) ? 1 : i+1
+  if input[i] == input[j]
+    global s += parse(Int,input[i])
+  end
+end
+
+println(s)
