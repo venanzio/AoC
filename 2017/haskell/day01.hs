a30cc3c35b2ae5daf21bc6fa41036fdd857e4df7
venanzio <venanzio@duplavis.com>
2022-01-19 10:55:29 +0000

    2017 day 1 haskell, part 1

---
+++ 2017/haskell/day01.hs
@@ -0,0 +1,43 @@
+-- Advent of Code 2017, day ?
+
+module Main where
+
+import System.Environment
+import Data.List
+import Data.Char
+
+import Control.Applicative
+import qualified Data.Map as M
+
+import FunParser
+import AoCTools
+
+main :: IO ()
+main = do
+  args <- getArgs
+  puzzle (head args)
+
+puzzle :: String -> IO ()
+puzzle fileName = do
+  input <- readFile fileName
+  let xs = parseAll digits input
+  putStrLn ("Part 1: " ++ show (part1 xs))
+  putStrLn ("Part 2: " ++ show (part2 xs))
+
+-- Part 1
+
+sumDig :: [Int] -> Int
+sumDig (x:xs@(y:_)) = if x==y then x + sumDig xs else sumDig xs
+sumDig _ = 0
+
+captcha :: [Int] -> Int
+captcha xs = if last xs == head xs then head xs + sumDig xs else sumDig xs
+
+
+part1 :: [Int] -> Int
+part1 = captcha
+
+-- Part 2
+
+part2 :: [Int] -> Int
+part2 _ = 2
