cf609cb0fe2c07d6d028dd437384f61a84c6b23e
venanzio <venanzio@duplavis.com>
2022-01-20 09:03:32 +0000

    day 2, part 1

--- 2017/julia/day02.jl
+++ 2017/julia/day02.jl
@@ -4,4 +4,16 @@ f = open("../input")
 input = readlines(f)
 close(f)
 
-print(input)
+function parseNums(s)
+  [parse(Int,n) for n in split(s)]
+end
+
+spreadsheet = [parseNums(s) for s in input]
+
+function diff(l)
+  maximum(l)-minimum(l)
+end
+
+checksum = sum([diff(l) for l in spreadsheet])
+
+println("part 1: ", checksum)
