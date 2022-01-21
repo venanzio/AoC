1c9e94b59c23b0482f6321966db2fa7b0447ffee
venanzio <venanzio@duplavis.com>
2022-01-21 16:20:39 +0000

    added day 11 file

---
+++ 2020/python/day11.py
@@ -0,0 +1,30 @@
+# Advent of Code 2020, Day 11
+
+f = open("../input")
+input = f.readlines()
+f.close()
+
+seats = re.split("\n *\n",input)
+print(str(seat))
+
+# Parsing the input
+
+length = len(input)
+width = len(input[0])
+
+
+print("length = " + str(length))
+print("width = " + str(width))
+
+print("[" + input[length-1] + "]")
+
+# Part 1
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
