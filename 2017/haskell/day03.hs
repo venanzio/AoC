5b3d64d71c22728c8bc164bcd6d8b0339b6ce5f9
venanzio <venanzio@duplavis.com>
2022-01-25 12:38:55 +0000

    square_lt function

--- 2017/haskell/day03.hs
+++ 2017/haskell/day03.hs
@@ -16,11 +16,22 @@ main :: IO ()
 main = do
   putStr("input number: ")
   input <- getLine
-  let x = read input :: Int
+  let x = read input
+  puzzle x
+
+input = 289326 :: Int
+
+puzzle :: Int -> IO ()
+puzzle x = do
   putStrLn ("Part 1: " ++ show (part1 x))
   putStrLn ("Part 2: " ++ show (part2 x))
 
 -- Part 1
+
+-- "inner" square, return n s.t. (2n-1)^2 < x <= (2n+1)^2
+square_lt :: Int -> Int
+square_lt x = sqlt 0
+  where sqlt n = if x <= (2*n+1)^2 then n else sqlt (n+1)
 
 part1 :: Int -> Int
 part1 _ = 1
