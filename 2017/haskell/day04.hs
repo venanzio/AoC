2d1c5e9db05eb5bb81d8c6ca3c5abc071dacb2db
venanzio <venanzio@duplavis.com>
2022-01-27 21:20:35 +0000

    2017 day 4 haskell done

--- 2017/haskell/day04.hs
+++ 2017/haskell/day04.hs
@@ -22,7 +22,6 @@ puzzle fileName = do
   putStrLn "Advent of Code 2017, day 4"
   input <- readFile fileName
   let xs = parseAll pInput input
-  putStrLn (show (last xs))
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
@@ -38,10 +37,19 @@ pInput = pLines pPhrase
 
 -- Part 1
 
+noDups :: (a->a->Bool) -> [a] -> Bool
+noDups eq xs = length xs == length (nubBy eq xs)
+
+countValid :: (a->a->Bool) -> [[a]] -> Int
+countValid eq = length . filter (noDups eq)
+
 part1 ::  [Phrase] -> Int
-part1 _ = 1
+part1 = countValid (==)
 
 -- Part 2
 
+anagram :: String -> String -> Bool
+anagram s1 s2 = sort s1 == sort s2
+
 part2 ::  [Phrase] -> Int
-part2 _ = 2
+part2 = countValid anagram
