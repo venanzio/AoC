8bccb439a396c847117b0bd2adefd1ac02b4fabb
venanzio <venanzio@duplavis.com>
2022-02-06 15:04:22 +0000

    Definition of "two ways lists" and move function

--- 2017/haskell/day05.hs
+++ 2017/haskell/day05.hs
@@ -22,18 +22,31 @@ puzzle fileName = do
   putStrLn "Advent of Code 2017, day 5"
   input <- readFile fileName
   let xs = parseAll pInput input
+  putStrLn(show xs)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
 
 pInput :: Parser [Int]
 pInput = some integer
 
 -- Part 1
+
+-- two ways lists
+type TwoList a = ([a],[a]) -- front and back in reverse order
+
+tlInit :: [a] -> TwoList a
+tlInit l = (l,[])
+
+move :: Int -> TwoList a -> Maybe (TwoList a)
+move 0 l = Just l
+move n (x:xs,ys) | n>0 = move (n-1) (xs,x:ys)
+move n (xs,y:ys) | n<0 = move (n+1) (y:xs,ys)
+move _ _ = Nothing
+
+
 
 part1 :: [Int] -> Int
 part1 _ = 1
