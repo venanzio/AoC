87a5a5b05b48a0ee1fbcbdb24175d107fd6463bd
venanzio <venanzio@duplavis.com>
2022-02-06 15:37:10 +0000

    Defined trip function, but goes out of bounds

--- 2017/haskell/day05.hs
+++ 2017/haskell/day05.hs
@@ -22,9 +22,10 @@ puzzle fileName = do
   putStrLn "Advent of Code 2017, day 5"
   input <- readFile fileName
   let xs = parseAll pInput input
-  putStrLn(show xs)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
+
+exMaze = mInit [0,3,0,1,-3]
 
 -- Parsing the input
 
@@ -35,21 +36,44 @@ pInput = some integer
 -- Part 1
 
 -- two ways lists
-type TwoList a = ([a],[a]) -- front and back in reverse order
+type Maze = ([Int],[Int]) -- front and back in reverse order
 
-tlInit :: [a] -> TwoList a
-tlInit l = (l,[])
+mInit :: [Int] -> Maze
+mInit l = (l,[])
 
-move :: Int -> TwoList a -> Maybe (TwoList a)
-move 0 l = Just l
-move n (x:xs,ys) | n>0 = move (n-1) (xs,x:ys)
-move n (xs,y:ys) | n<0 = move (n+1) (y:xs,ys)
-move _ _ = Nothing
+mSound :: Maze -> Bool
+mSound = not . null
 
+mMove :: Int -> Maze -> Maybe (Maze)
+mMove 0 m | mSound m = Just m
+mMove n (x:xs,ys) | n>0 = mMove (n-1) (xs,x:ys)
+mMove n (xs,y:ys) | n<0 = mMove (n+1) (y:xs,ys)
+mMove _ _ = Nothing
 
+mRead :: Maze -> Int
+mRead (x:_,_) = x
+mRead _ = error "out of bounds"
+
+mWrite :: Int -> Maze -> Maze
+mWrite x (_:xs,ys) = (x:xs,ys)
+mWrite _ _ = error "out of bounds"
+
+mStep :: Maze -> Maybe Maze
+mStep m = let x = mRead m
+          in case mMove x m of
+               Nothing -> Nothing
+               Just m' -> Just (mWrite (x+1) m')
+
+mTrip :: Maze -> Int
+mTrip m =
+  let x = mRead m
+      m1 = mWrite (x+1) m
+  in case mMove x m1 of
+       Just m2 -> (mTrip m2) + 1
+       Nothing -> 0
 
 part1 :: [Int] -> Int
-part1 _ = 1
+part1 = mTrip . mInit
 
 -- Part 2
 
