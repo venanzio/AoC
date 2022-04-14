f61af0616820ff06d9259fcb9e96c603f104adb6
venanzio <venanzio@duplavis.com>
2022-04-14 18:32:01 +0100

    2020 python: skeleton for days 13, 14, 15 (I fell behind)

---
+++ 2020/python/day15.py
@@ -0,0 +1,20 @@
+print("Advent of Code 2020, Day 15")
+
+f = open("../input")
+input = f.read().strip()
+f.close()
+
+# Parsing the input
+
+# ...
+
+# Part 1
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
