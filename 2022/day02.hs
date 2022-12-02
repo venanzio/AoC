7f0c2d555ab26c5f9df21b2436b8f1bb63a382c5
venanzio <venanzio@duplavis.com>
2022-12-02 05:27:05 +0000

    Solutions Day 2

--- 2022/day02.hs
+++ 2022/day02.hs
@@ -27,18 +27,52 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+pData :: Parser (String,String)
+pData = do opponent <- word
+           you <- word
+           return (opponent,you)
 
-pInput :: Parser [()]
-pInput = pLines pData
+pInput :: Parser [(String,String)]
+pInput = many pData
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+shape :: String -> Int
+shape "X" = 1
+shape "Y" = 2
+shape "Z" = 3
+
+outcome :: String -> String -> Int
+outcome "C" "X" = 6
+outcome "B" "Z" = 6
+outcome "A" "Y" = 6
+outcome "A" "Z" = 0
+outcome "C" "Y" = 0
+outcome "B" "X" = 0
+outcome a x = 3
+
+guide :: (String,String) -> Int
+guide (a,x) = shape x + outcome a x
+
+
+part1 :: [(String,String)] -> Int
+part1 = sum . map guide
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+choose :: String -> String -> String
+choose "A" "X" = "Z"
+choose "A" "Y" = "X"
+choose "A" "Z" = "Y"
+choose "B" "X" = "X"
+choose "B" "Y" = "Y"
+choose "B" "Z" = "Z"
+choose "C" "X" = "Y"
+choose "C" "Y" = "Z"
+choose "C" "Z" = "X"
+
+
+
+
+part2 :: [(String,String)] -> Int
+part2 = part1 . map (\(a,x) -> (a,choose a x))
