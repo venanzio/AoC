d1e90e9b8b1ba59458290fe61cfeefcdfb15ea59
venanzio <venanzio@duplavis.com>
2022-12-04 05:17:29 +0000

    Solution Day 4

--- 2022/day04.hs
+++ 2022/day04.hs
@@ -27,18 +27,39 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+type Range = (Int,Int)
 
-pInput :: Parser [()]
+pRange :: Parser Range
+pRange = do start <- integer
+            symbol "-"
+            end <- integer
+            return (start,end)
+
+pData :: Parser (Range,Range)
+pData = do first <- pRange
+           symbol ","
+           second <- pRange
+           return (first,second)
+
+pInput :: Parser [(Range,Range)]
 pInput = pLines pData
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+contained :: (Range,Range) -> Bool
+contained ((s1,e1),(s2,e2)) = s1 >= s2 && e1<=e2
+
+eitherContained :: (Range,Range) -> Bool
+eitherContained (r1,r2) = contained (r1,r2) || contained (r2,r1)
+
+part1 :: [(Range,Range)] -> Int
+part1 = length . filter eitherContained
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+overlap :: (Range,Range) -> Bool
+overlap ((s1,e1),(s2,e2)) = not (e1<s2 || e2<s1)
+
+part2 :: [(Range,Range)] -> Int
+part2 = length . filter overlap
+
