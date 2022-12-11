e52f5b22dccbc941973b9ef70b36ccf1340b4c9b
venanzio <venanzio@duplavis.com>
2022-12-11 07:04:23 +0000

    Day 11 part 1 for part 2, worry levels grow too much, must find a way to keep them down

--- 2022/day11.hs
+++ 2022/day11.hs
@@ -27,18 +27,95 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+data Monkey = Monkey {
+  mItems :: [Integer],
+  mOperation :: Integer -> Integer,
+  mTest :: Integer -> Int,  -- indicating to which monkey to throw
+  mActivity :: Int
+  }
 
-pInput :: Parser [()]
-pInput = pLines pData
+type Monkeys = M.Map Int Monkey
+
+pOp :: Parser (Integer -> Integer -> Integer)
+pOp = (symbol "+" >> return (+)) <|> (symbol "*" >> return (*)) 
+
+pOperand :: Parser (Integer -> Integer)
+pOperand = (symbol "old" >> return id)
+           <|>
+           do x <- natural
+              return (\_ -> x)
+
+pOperation :: Parser (Integer -> Integer)
+pOperation = do a1 <- pOperand
+                op <- pOp
+                a2 <- pOperand
+                return (\x -> op (a1 x) (a2 x))
+
+pMonkeyN :: Parser Int
+pMonkeyN = do symbol "Monkey"
+              n <- natural
+              symbol ":"
+              return n
+
+pMonkey :: Parser Monkey
+pMonkey =
+  do symbol "Starting items:"
+     items <- manySepStr natural ","
+     symbol "Operation: new ="
+     operation <- pOperation
+     symbol "Test: divisible by"
+     testV <- natural
+     symbol "If true: throw to monkey"
+     monkey1 <- natural
+     symbol "If false: throw to monkey"
+     monkey2 <- natural
+     return (Monkey {mItems = items,
+                     mOperation = operation,
+                     mTest = \x -> if x `mod` testV == 0
+                                     then monkey1 else monkey2,
+                     mActivity = 0})
+              
+pMonkeys :: Parser Monkeys
+pMonkeys = do n <- pMonkeyN
+              m <- pMonkey
+              ms <- pMonkeys
+              return (M.insert n m ms)
+           <|> return M.empty
+
+pInput :: Parser Monkeys
+pInput = pMonkeys
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+monkeyThrow :: Int -> Monkeys -> Monkeys
+monkeyThrow n ms =
+  let m = ms M.! n
+  in case mItems m of
+    [] -> ms
+    (x:xs) -> let x' = mOperation m x -- `div` 3
+                  n1 = (mTest m x')
+                  m1 = ms M.! n1
+              in M.insert n (m {mItems = xs, mActivity = mActivity m +1})
+                   (M.insert n1 (m1 {mItems = x':mItems m1}) ms)
+
+monkeyTurn :: Int -> Monkeys -> Monkeys
+monkeyTurn n ms = if (mItems (ms M.! n)) == []
+                    then ms
+                    else monkeyTurn n (monkeyThrow n ms)
+
+monkeyRound :: Monkeys -> Monkeys
+monkeyRound ms = M.foldlWithKey (\ms n _ -> monkeyTurn n ms) ms ms
+
+monkeyBusiness :: Monkeys -> Int
+monkeyBusiness ms =
+  let as = reverse $ sort $ M.elems $ M.map mActivity ms
+  in as!!0 * as!!1
+  
+part1 :: Monkeys -> Int
+part1 = monkeyBusiness . nIter monkeyRound 20
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+part2 :: Monkeys -> Int
+part2 = monkeyBusiness . nIter monkeyRound 10000
+
