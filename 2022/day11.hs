ab627961a8b043ba01501c1cf52e5b77f3eced60
venanzio <venanzio@duplavis.com>
2022-12-11 08:26:59 +0000

    Day 11: made the solution more general

--- 2022/day11.hs
+++ 2022/day11.hs
@@ -22,7 +22,6 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
-  putStrLn (show $ map mTestV $ M.elems xs)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
@@ -34,7 +33,6 @@ data Monkey = Monkey {
   mTestV :: Int,
   mTestY :: Int,
   mTestN  :: Int,
---  mTest :: Int -> Int,  -- indicating to which monkey to throw
   mActivity :: Int
   }
 
@@ -42,9 +40,6 @@ mTest :: Monkey -> Int -> Int
 mTest m x = if x `mod` (mTestV m) == 0
               then (mTestY m) else (mTestN m)
 
-bigNum = product [5,2,13,7,19,11,3,17]
-         -- product [23,19,13,17]
-         
 type Monkeys = M.Map Int Monkey
 
 pOp :: Parser (Int -> Int -> Int)
@@ -85,8 +80,6 @@ pMonkey =
                      mTestV = testV,
                      mTestY = monkey1,
                      mTestN = monkey2,
---                     mTest = \x -> if x `mod` testV == 0
---                                     then monkey1 else monkey2,
                      mActivity = 0})
               
 pMonkeys :: Parser Monkeys
@@ -106,7 +99,7 @@ monkeyThrow n ms =
   let m = ms M.! n
   in case mItems m of
     [] -> ms
-    (x:xs) -> let x' = mOperation m x `mod` bigNum -- `div` 3
+    (x:xs) -> let x' = mOperation m x
                   n1 = (mTest m x')
                   m1 = ms M.! n1
               in M.insert n (m {mItems = xs, mActivity = mActivity m +1})
@@ -124,12 +117,21 @@ monkeyBusiness :: Monkeys -> Int
 monkeyBusiness ms =
   let as = reverse $ sort $ M.elems $ M.map mActivity ms
   in as!!0 * as!!1
+
+reviseOperations1 :: Monkeys -> Monkeys
+reviseOperations1 =
+  M.map (\m -> m { mOperation = \x -> (mOperation m) x `div` 3 })
   
 part1 :: Monkeys -> Int
-part1 = monkeyBusiness . nIter monkeyRound 20
+part1 = monkeyBusiness . nIter monkeyRound 20 . reviseOperations1
 
 -- Part 2
 
+reviseOperations2 :: Monkeys -> Monkeys
+reviseOperations2 ms =
+  let bigNum = product $ map mTestV $ M.elems ms
+  in M.map (\m -> m { mOperation = \x -> (mOperation m) x `mod` bigNum }) ms
+
 part2 :: Monkeys -> Int
-part2 = monkeyBusiness . nIter monkeyRound 10000
+part2 = monkeyBusiness . nIter monkeyRound 10000 . reviseOperations2
 
