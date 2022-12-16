96766d6c970751f28ecb4b4abbd955b9364a2f40
venanzio <venanzio@duplavis.com>
2022-12-16 07:40:26 +0000

    Day 16 parsing the input

--- 2022/day16.hs
+++ 2022/day16.hs
@@ -22,23 +22,46 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
+  putStrLn (show $ xs M.! "VJ")
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+data Valve = Valve {
+  vFlowRate :: Int,
+  vTunnels :: [String]
+  }
+  deriving Show
 
-pInput :: Parser [()]
-pInput = pLines pData
+type Cave = M.Map String Valve
+
+pValves :: Parser [String]
+pValves = do symbol "; tunnels lead to valves"
+             someSepStr word ","
+          <|>
+          do symbol "; tunnel leads to valve"
+             v <- word
+             return [v]
+
+pData :: Parser (String,Valve)
+pData = do symbol "Valve"
+           vName <- word
+           symbol "has flow rate="
+           fRate <- integer
+           vs <- pValves
+           return (vName,Valve fRate vs)
+
+pInput :: Parser Cave
+pInput = do vs <- pLines pData
+            return (M.fromList vs)
 
 -- Part 1
 
-part1 :: [()] -> Int
+part1 :: Cave -> Int
 part1 _ = 1
 
 -- Part 2
 
-part2 :: [()] -> Int
+part2 :: Cave -> Int
 part2 _ = 2
