e5ec029e3f1446a505c1b43f43564e460e038da5
venanzio <venanzio@duplavis.com>
2022-12-17 09:14:24 +0000

    Still incorrect: rocks not folling into "holes"

--- 2022/day17.hs
+++ 2022/day17.hs
@@ -22,7 +22,6 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
-  putStrLn (show rocks)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
@@ -69,15 +68,23 @@ mvRight = shiftRock (0,1)
 leftWContact :: Rock -> Bool
 leftWContact = any (\(x,_) -> x==0)
 
+leftRContact :: Rock -> Rock -> Bool
+leftRContact r0 r1 = any touch r0 where
+  touch (x,y) = any (\(x0,y0) -> x==x0+1 && y==y0) r1
+
 rightWContact :: Rock -> Bool
 rightWContact = any (\(x,_) -> x==6)
+
+rightRContact :: Rock -> Rock -> Bool
+rightRContact r0 r1 = any touch r0 where
+  touch (x,y) = any (\(x0,y0) -> x==x0-1 && y==y0) r1
 
 hitBottom :: Rock -> Bool
 hitBottom = any (\(_,y) -> y==1)
 
 rockContact :: Rock -> Rock -> Bool
 rockContact r0 r1 = any touch r0 where
-  touch (x,y) = any (\(x0,y0) -> x0==x && y0==x+1) r1
+  touch (x,y) = any (\(x0,y0) -> x==x0 && y==y0+1) r1
 
 towerHeight :: Rock -> Int
 towerHeight = maximumB 0 . map snd
@@ -86,27 +93,32 @@ rockFall :: Rock -> Queue Char -> Rock -> (Queue Char,Rock)
 rockFall r wind tower =
   let (gust,wind') = nextQ wind
       r1 = case gust of
-             '<' -> if leftWContact r then r else mvLeft r
-             '>' -> if rightWContact r then r else mvRight r
+             '<' -> if leftWContact r || leftRContact r tower
+                      then r else mvLeft r
+             '>' -> if rightWContact r || rightRContact r tower
+                      then r else mvRight r
       r2 = mvDown r1
   in if rockContact r1 tower
        then (wind', r1 ++ tower)
-       else if rockContact r2 tower || hitBottom r2
+       else if rockContact r2 tower -- || hitBottom r2
               then (wind', r2 ++ tower)
               else rockFall r2 wind' tower
   
 
 fallingRocks :: Queue Rock -> Queue Char -> Rock -> Int -> Int
-fallingRocks _ _ tower 0 = error (show tower) -- towerHeight tower
+fallingRocks _ _ tower 0 = towerHeight tower
 fallingRocks rs wind tower n =
   let (r,rs') = nextQ rs
       h = towerHeight tower
       r0 = shiftRock (2,h+3) r
       (wind',tower') = rockFall r0 wind tower
   in fallingRocks rs' wind' tower' (n-1)
-      
+
+initTower :: Rock
+initTower = zip [0..6] (repeat 0)
+
 part1 :: Queue Char -> Int
-part1 wind = fallingRocks rocks wind [] 2022
+part1 wind = fallingRocks rocks wind initTower 3
 
 -- Part 2
 
