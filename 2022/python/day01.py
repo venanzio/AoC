7e54b89c62de6cb563d240d7de4ae318369dca7f
venanzio <venanzio@duplavis.com>
2023-08-18 12:11:01 +0100

    2022 day 1 python solution

--- 2022/python/day01.py
+++ 2022/python/day01.py
@@ -9,15 +9,29 @@ f.close()
 
 # Parsing the input
 
-# ...
+# split a string in blocks separated by empty lines
+def blocks(s):
+  return (s.split('\n\n'))
+
+# list of calories for one elf
+def elfcal(s):
+  return [int(x) for x in s.split()]
+
+# calories for each food item for each elf
+foodcal = [elfcal(s) for s in blocks(input)]
 
 # Part 1
 
 print("Part 1: ")
 
+calories = [sum(xs) for xs in foodcal]
+
+print(max(calories))
 
 # Part 2
 
 print("Part 2: ")
   
+calories.sort(reverse=True)
 
+print(sum(calories[0:3]))
