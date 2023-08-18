9b442326017cce12e7cb206c4a612349e359cff5
venanzio <venanzio@duplavis.com>
2023-08-18 11:31:49 +0100

    2022 python: skeleton file and started day 1

---
+++ 2022/python/day00.py
@@ -0,0 +1,23 @@
+# Advent of Code 2022, day ?
+#   Venanzio Capretta
+
+print("Advent of Code 2022, Day ?")
+
+f = open("../input")
+input = f.read().strip()
+f.close()
+
+# Parsing the input
+
+# ...
+
+# Part 1
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
---
+++ 2022/python/day01.py
@@ -0,0 +1,23 @@
+# Advent of Code 2022, day 1
+#   Venanzio Capretta
+
+print("Advent of Code 2022, Day 1")
+
+f = open("../input")
+input = f.read().strip()
+f.close()
+
+# Parsing the input
+
+# ...
+
+# Part 1
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
