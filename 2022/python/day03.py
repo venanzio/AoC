b9fe5518b3705d2d356c676ae610fd5735a58f26
venanzio <venanzio@duplavis.com>
2023-08-21 21:33:55 +0100

    day 3 part 1

--- 2022/python/day03.py
+++ 2022/python/day03.py
@@ -9,12 +9,27 @@ f.close()
 
 # Parsing the input
 
-# ...
+rucksacks = input.splitlines()
 
 # Part 1
 
-print("Part 1: ")
+def common(s1,s2):
+  return ''.join(set(s1).intersection(s2))
 
+def wrong_item(r):
+  n = len(r) // 2
+  r1 = r[:n]
+  r2 = r[n:]
+  return common(r1,r2)
+
+def priority(c):
+  if c.islower():
+    return (ord(c)-ord('a')+1)
+  else:
+    return (ord(c)-ord('A')+27)
+
+print("Part 1: ")
+print(sum([priority(wrong_item(r)) for r in rucksacks]))
 
 # Part 2
 
