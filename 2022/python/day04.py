146ba00a407f1fd71ead97937e1285944fcde2ac
venanzio <venanzio@duplavis.com>
2023-09-18 12:33:04 +0100

    day 4 part 1 completed

--- 2022/python/day04.py
+++ 2022/python/day04.py
@@ -11,20 +11,22 @@ f.close()
 
 def parsePair(s):
   vs = s.replace('-',',').split(',')
-  return ((vs[0],vs[1]),(vs[2],vs[3]))
+  return ((int(vs[0]),int(vs[1])),(int(vs[2]),int(vs[3])))
 
 pairRange = list(map(parsePair,input.splitlines()))
 
 # Part 1
 
-def rangeContained(r1,r2):
-  return(r1[0]>=r2[0] and r1[1]<=r2[1])
+def rangeContained(r0,r1):
+  return(r0[0] >= r1[0] and r0[1] <= r1[1])
 
-contained = list(filter(lambda rs: rangeContained(rs[0],rs[1]) or rangeContained(rs[1],rs[0]),
-                        pairRange))
+contained = 0
+for (r0,r1) in pairRange:
+  if rangeContained(r0,r1) or rangeContained(r1,r0):
+    contained += 1
 
 print("Part 1: ")
-print(len(contained))
+print(contained)
 
 # Part 2
 
