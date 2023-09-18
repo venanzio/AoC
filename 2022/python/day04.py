8daf4d64ab511aceb8f5cc9a0d31e023c44c4243
venanzio <venanzio@duplavis.com>
2023-09-18 10:53:06 +0100

    2022 python: skeleton for day 4

---
+++ 2022/python/day04.py
@@ -0,0 +1,25 @@
+# Advent of Code 2022, day 4
+#   Venanzio Capretta
+
+print("Advent of Code 2022, Day 4")
+
+f = open("../input04")
+input = f.read().strip()
+f.close()
+
+# Parsing the input
+
+# ...
+
+print(input)
+
+# Part 1
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
