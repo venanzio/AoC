7f8f94cceef4a4a95d5fc06d0636e003f26c2baa
venanzio <venanzio@duplavis.com>
2023-10-27 15:51:19 +0100

    day 5 python: split the input

--- 2022/python/day05.py
+++ 2022/python/day05.py
@@ -4,13 +4,26 @@
 print("Advent of Code 2022, Day 5")
 
 f = open("../input05")
-input = f.read().strip()
+input = f.readlines()
 f.close()
 
 # Parsing the input
 
-# ...
+def numberline(s):
+  return (s[1]=='1')
 
+sinput = []
+while not numberline(input[0]):
+  sinput.append(input[0])
+  input.pop(0)
+
+# eliminates numbers and blanks
+input.pop(0)
+input.pop(0)
+
+print(sinput)
+print(input[0])
+ 
 # Part 1
 
 print("Part 1: ")
