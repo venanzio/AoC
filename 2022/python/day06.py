304b7a54666532cf0f86b2f601432f91f030dadb
venanzio <venanzio@duplavis.com>
2023-11-08 15:24:51 +0000

    2022 python day 6 skeleton

---
+++ 2022/python/day06.py
@@ -0,0 +1,23 @@
+# Advent of Code 2022, day 6
+#   Venanzio Capretta
+
+print("Advent of Code 2022, Day 6")
+
+f = open("../input06")
+input = f.read().strip()
+f.close()
+
+# Part 1
+
+
+def marker(s):
+  return True
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
