588b84faae880c54244bbef4906cfded29224029
venanzio <venanzio@duplavis.com>
2023-11-09 17:07:43 +0000

    part 2 finished (simple error of function name)

--- 2022/python/day07.py
+++ 2022/python/day07.py
@@ -94,9 +94,11 @@ free_space = 70000000 - fs.get_size()
 needed_space = 30000000 - free_space
 
 def all_sizes(fs):
-  ch_sz = sum([sizes(x) for x in fs.children if x.is_file==False],[])
+  ch_sz = sum([all_sizes(x) for x in fs.children if x.is_file==False],[])
   ch_sz.append(fs.size)
   return ch_sz
+
+print(all_sizes(fs))
 
 big_sizes = [sz for sz in all_sizes(fs) if sz >= needed_space]
 big_sizes.sort()
