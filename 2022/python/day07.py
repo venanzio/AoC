463ad091cd155c84ba84f9360d1e0a114bee2323
venanzio <venanzio@duplavis.com>
2023-11-09 11:33:42 +0000

    defined tree data class

--- 2022/python/day07.py
+++ 2022/python/day07.py
@@ -3,11 +3,25 @@
 
 print("Advent of Code 2022, Day 7")
 
-f = open("../input")
+f = open("../input07")
 input = f.read().strip()
 f.close()
 
 # Parsing the input
+
+class filesystem:
+  def __init__(self,name,size):
+    self.name = name
+    self.size = size
+    self.children = []
+
+  def __str__(self):
+    return f"{self.name} ({self.size}) {self.children}"
+
+
+fs = filesystem("/",-1)
+
+print(fs)
 
 # ...
 
