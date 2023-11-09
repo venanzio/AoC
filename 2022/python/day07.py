ed4d8d5eaf73edd44e21cae77dded6268571f217
venanzio <venanzio@duplavis.com>
2023-11-09 11:39:48 +0000

    created "node" and "child" subclasses

--- 2022/python/day07.py
+++ 2022/python/day07.py
@@ -10,14 +10,19 @@ f.close()
 # Parsing the input
 
 class filesystem:
-  def __init__(self,name,size):
+  def __init__(self,name):
     self.name = name
-    self.size = size
+    self.size = -1
     self.children = []
 
   def __str__(self):
     return f"{self.name} ({self.size}) {self.children}"
 
+class node(filesystem):
+  pass
+
+class leaf(filesystem):
+  pass
 
 fs = filesystem("/",-1)
 
