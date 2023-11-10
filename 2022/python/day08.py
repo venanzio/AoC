5a2a94f60f7316c5741e3f234f37d5dc7f8c269b
venanzio <venanzio@duplavis.com>
2023-11-10 19:22:08 +0000

    visible doesn't work because indices wrap around

--- 2022/python/day08.py
+++ 2022/python/day08.py
@@ -17,21 +17,28 @@ def num_row(s):
 
 grid = [num_row(s) for s in input]
 
+boundy = len(grid)
+boundx = len(grid[0])
+
 # Part 1
 
 # a direction is a couple of (dx,dy) increments
 
-def visible(x,y,dx,dy)
+def in_bounds(x,y):
+  return (0 <= x < boundx and 0 <= y < boundy)
+
+def visible(x,y,dx,dy):
   ix = x+dx
   iy = y+dy
-  while not is_None(grid[iy][ix]):
+  while in_bounds(ix,iy):
     if grid[y][x] <= grid[iy][ix]:
       return False
   return True
 
 print("Part 1: ")
 
-
+print(in_bounds(-1,-1))
+print(visible(0,0,1,0))
 
 # Part 2
 
