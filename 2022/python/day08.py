1a97ffc2eba981ddd7a3584455844013148c90b2
venanzio <venanzio@duplavis.com>
2023-11-10 18:53:13 +0000

    generatin the grid and cheching visibility in one direction

--- 2022/python/day08.py
+++ 2022/python/day08.py
@@ -9,11 +9,28 @@ f.close()
 
 # Parsing the input
 
-print(input)
+def num_row(s):
+  r = []
+  for x in s:
+    r.append(int(x))
+  return r
+
+grid = [num_row(s) for s in input]
 
 # Part 1
 
+# a direction is a couple of (dx,dy) increments
+
+def visible(x,y,dx,dy)
+  ix = x+dx
+  iy = y+dy
+  while not is_None(grid[iy][ix]):
+    if grid[y][x] <= grid[iy][ix]:
+      return False
+  return True
+
 print("Part 1: ")
+
 
 
 # Part 2
