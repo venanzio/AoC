bedba6a4ea3cfeb4cab52e9b8e36a82ea527aabc
venanzio <venanzio@duplavis.com>
2023-11-11 11:00:20 +0000

    2022 python day 9 skeleton

---
+++ 2022/python/day09.py
@@ -0,0 +1,25 @@
+# Advent of Code 2022, day 9
+#   Venanzio Capretta
+
+print("Advent of Code 2022, Day 9")
+
+f = open("../input09")
+input = f.read().splitlines()
+f.close()
+
+# Parsing the input
+
+def move(s):
+  (mv,steps) = s.split(' ',1)
+  return (mv,int(steps))
+
+# Part 1
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
