80e884bd137553bddaf6608a16e2a7d1ad4ad753
venanzio <venanzio@duplavis.com>
2023-11-12 09:04:23 +0000

    fixed the cycle count: still gives wrong results

--- 2022/python/day10.py
+++ 2022/python/day10.py
@@ -19,19 +19,27 @@ for s in input:
 # Part 1
 
 
-action_range = range(0,len(program)+3)
+#action_range = range(0,len(program)+3)
 
-actions = [0 for i in action_range]
+#actions = [0 for i in action_range]
+#for i in range(len(program)):
+#  if program[i][0] == 'addx':
+#    actions[i+1] += program[i][1]
+
+actions = [0]
 for i in range(len(program)):
+  actions.append(0)
   if program[i][0] == 'addx':
-    actions[i+1] += program[i][1]
+    actions.append(program[i][1])
+
+print(actions)
 
 def signal_strength(c,x):
   return c*x
 
 registerX = 1
 sig_sum = 0
-for cycle in action_range:
+for cycle in range(len(actions)):
   if (cycle-20)%40 == 0:
     sig_sum += signal_strength(cycle,registerX)
     print('cycle ',cycle,' - sum = ',sig_sum)
