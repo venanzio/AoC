696b03a529d449b81f3fe72179d892c088425970
venanzio <venanzio@duplavis.com>
2023-11-12 08:23:15 +0000

    parsing of input

--- 2022/python/day10.py
+++ 2022/python/day10.py
@@ -4,14 +4,28 @@
 print("Advent of Code 2022, Day 10")
 
 f = open("../input10")
-input = f.read().strip()
+input = f.read().splitlines()
 f.close()
 
 # Parsing the input
 
-# ...
+program = []
+for s in input:
+  if s[0:4] == 'noop':
+    program.append(('noop',0))
+  else:
+    program.append(('addx',int(s[4:])))
 
 # Part 1
+
+cycle = 1
+registerX = 1
+
+
+def signal_strength():
+  return cycle*registerX
+
+print(signal_strength())
 
 print("Part 1: ")
 
