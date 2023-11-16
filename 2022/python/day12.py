35bd9464680df26a63be3b02d8d4a6527673484c
venanzio <venanzio@duplavis.com>
2023-11-16 16:31:18 +0000

    2022 day 12 python skeleton

---
+++ 2022/python/day12.py
@@ -0,0 +1,23 @@
+# Advent of Code 2022, day 12
+#   Venanzio Capretta
+
+print("Advent of Code 2022, Day 12")
+
+f = open("../input12")
+input = f.read()
+f.close()
+
+# Parsing the input
+
+# ...
+
+# Part 1
+
+print("Part 1: ")
+
+
+# Part 2
+
+print("Part 2: ")
+  
+
