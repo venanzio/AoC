5b812b91b5e464b7426be4b53ec149d131c7afb8
venanzio <venanzio@duplavis.com>
2023-11-16 17:03:35 +0000

    Started library for Dijkstra

---
+++ 2022/python/dijkstra.py
@@ -0,0 +1,9 @@
+# Dijkstra's algorithm
+# Venanzio Capretta, 2023
+
+# a graph is represented as a adjacencey list
+#  dictionary vertex -> [vertex]
+
+gr = {1:[2,3,4], 2:[1,4], 3:[], 4:[2]}
+
+print(gr.keys())
