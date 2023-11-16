86a290c26dd90c9f711fb1cb4b6228c8e6db2d60
venanzio <venanzio@duplavis.com>
2023-11-16 17:50:35 +0000

    started to impkement dijkstra

--- 2022/python/dijkstra.py
+++ 2022/python/dijkstra.py
@@ -12,13 +12,15 @@ def min_dic(dic):
 
 def dijkstra(gr,s,t):
   vertices = {v:None for v in gr.keys()}
-  print(vertices)
-  print(min(vertices.values()))
-
+  vertices[s] = 0
+  visited = { }
+  while vertices:
+    u = min_dic(vertices)
+    visited[u] = vertices.pop(u) 
+    for v in (v for v in vertices if adjecent(gr,u,v)):
+      pass
   return None
 
-
-print(min_dic({1:20,2:24,3:7}))
 
 gr = {1:[2,3,4], 2:[1,4], 3:[], 4:[2]}
 
