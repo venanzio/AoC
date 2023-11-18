c0a9cb37be6d3efd1ae16fcae61b4453914ffd6f
venanzio <venanzio@duplavis.com>
2023-11-18 14:51:23 +0000

    computation of rock lines

--- 2022/python/day14.py
+++ 2022/python/day14.py
@@ -10,6 +10,20 @@ f.close()
 
 # Parsing the input
 
+def rock_line(p1,p2):
+  if p1[0]==p2[0]:
+    st = min(p1[1],p2[1])
+    en = max(p1[1],p2[1])
+    return {(p1[0],x) for x in range(st,en+1)}
+  elif p1[1]==p2[1]:
+    st = min(p1[0],p2[0])
+    en = max(p1[0],p2[0])
+    return {(x,p1[1]) for x in range(st,en+1)}
+  else:
+    return None
+
+print(rock_line((498,6),(496,6)))
+
 def rock_path(s):
   return P.lst_sep(lambda s: P.pair(P.num,P.num,s), '->',s)
   
