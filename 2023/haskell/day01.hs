83486c887630c9b3778fc7a5a3cc5498ac5c4fdf
venanzio <venanzio@duplavis.com>
2023-12-01 06:08:29 +0000

    da==2023 day 1 done

--- 2023/haskell/day01.hs
+++ 2023/haskell/day01.hs
@@ -27,18 +27,58 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+pData :: Parser String
+pData = word
 
-pInput :: Parser [()]
+pInput :: Parser [String]
 pInput = pLines pData
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+calibration :: String -> Int
+calibration s = firstDigit s * 10 + firstDigit (reverse s)
+
+firstDigit :: String -> Int
+firstDigit (x:xs) = if isDigit x then (read [x])
+                                 else firstDigit xs
+
+part1 :: [String] -> Int
+part1 = sum . map calibration
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+wDigits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+
+readDigit :: String -> Int
+readDigit w = (unJust $ elemIndex w wDigits) + 1
+
+
+
+bDigit :: String -> Maybe Int
+bDigit = bDigitAux wDigits
+
+bDigitAux :: [String] -> String -> Maybe Int
+bDigitAux [] s = if isDigit (s!!0) then Just (read [s!!0]) else Nothing
+bDigitAux (d:ds) s = if take (length d) s == d
+            then Just (readDigit d)
+            else bDigitAux ds s
+
+wordDigits :: String -> [Int]
+wordDigits [] = []
+wordDigits xs = case bDigit xs of
+  Just n -> n : wordDigits (tail xs)
+  Nothing -> wordDigits (tail xs)
+
+first xs = xs!!0
+
+-- last xs = xs!!(length xs - 1)
+
+wCalibration :: String -> Int
+wCalibration s =
+  let digs = wordDigits s
+  in (first digs) * 10 + (last digs)
+  
+
+part2 :: [String] -> Int
+part2 = sum . map wCalibration
