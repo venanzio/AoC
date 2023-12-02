d21007f66c5e992c01999977ab6df9e6f460ea3e
venanzio <venanzio@duplavis.com>
2023-12-02 11:53:32 +0000

    cleared unneeded function and restructured

--- 2023/haskell/day02.hs
+++ 2023/haskell/day02.hs
@@ -17,9 +17,9 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
-  putStrLn (show (xs!!0))
-  putStrLn ("Part 1: " ++ show (part1 xs))
-  putStrLn ("Part 2: " ++ show (part2 xs))
+      cs = map minColours xs
+  putStrLn ("Part 1: " ++ show (part1 cs))
+  putStrLn ("Part 2: " ++ show (part2 cs))
 
 -- Parsing the input
 
@@ -59,6 +59,15 @@ pInput= many (do symbol "Game"
                  symbol ":"
                  pGame)
 
+-- minimal number of colour cubes needed in a game
+minColours :: [Colours] -> Colours
+minColours [] = noColours
+minColours (c:cs) = let c0 = minColours cs in
+  Colours { cRed = max (cRed c) (cRed c0)
+          , cGreen = max (cGreen c) (cGreen c0)
+          , cBlue = max (cBlue c) (cBlue c0)
+          }
+  
 -- Part 1
 
 maxRed = 12
@@ -68,38 +77,19 @@ maxBlue = 14
 possibleCols :: Colours -> Bool
 possibleCols c = (cRed c <= maxRed) && (cGreen c <= maxGreen) && (cBlue c <= maxBlue)
 
-possibleGame :: [Colours] -> Bool
-possibleGame = all possibleCols
-
-possible :: [[Colours]] -> Int
-possible = possibleAux 1 0 where
-  possibleAux n s [] = s
-  possibleAux n s (g:gs) =
-    if (possibleGame g)
-    then possibleAux (n+1) (s+n) gs
-    else possibleAux (n+1) s gs
-
 possibleSum :: Int -> Int -> [Colours] -> Int
 possibleSum s n [] = s
 possibleSum s n (g:gs) =
   if possibleCols g then possibleSum (s+n) (n+1) gs
                     else possibleSum s (n+1) gs
 
-part1 :: [[Colours]] -> Int
-part1 = possibleSum 0 1 . map minColours 
+part1 :: [Colours] -> Int
+part1 = possibleSum 0 1 
 
 -- Part 2
-
-minColours :: [Colours] -> Colours
-minColours [] = noColours
-minColours (c:cs) = let c0 = minColours cs in
-  Colours { cRed = max (cRed c) (cRed c0)
-          , cGreen = max (cGreen c) (cGreen c0)
-          , cBlue = max (cBlue c) (cBlue c0)
-          }
 
 power :: Colours -> Int
 power c = (cRed c)*(cGreen c)*(cBlue c)
 
-part2 :: [[Colours]] -> Int
-part2 = sum . map (power . minColours)
+part2 :: [Colours] -> Int
+part2 = sum . map power
