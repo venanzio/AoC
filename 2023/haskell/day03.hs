b76fc287472b72acbb405fb50e14ce5bc4107525
venanzio <venanzio@duplavis.com>
2023-12-03 05:44:04 +0000

    reading coordinates of all symbols

--- 2023/haskell/day03.hs
+++ 2023/haskell/day03.hs
@@ -21,24 +21,32 @@ main = do
 puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
-  let xs = parseAll pInput input
-  putStrLn ("Part 1: " ++ show (part1 xs))
-  putStrLn ("Part 2: " ++ show (part2 xs))
+  let ls = lines input
+      pnums = partNums ls
+      symbs = symbols ls
+  putStrLn (show pnums)
+  putStrLn ("Part 1: " ++ show (part1 pnums symbs))
+  putStrLn ("Part 2: " ++ show (part2 pnums symbs))
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
-
-pInput :: Parser [()]
-pInput = pLines pData
+partNums :: [String] -> [(Int,Int,Int)]
+partNums = undefined
+    
+symbols :: [String] -> [(Int,Int)]
+symbols ls = symCoords 0 0 where
+  symCoords x y | y >= length ls      = []
+                | x >= length (ls!!y) = symCoords 0 (y+1)
+                | ls!!y!!x == '.' || isDigit (ls!!y!!x)
+                                      = symCoords (x+1) y
+                | otherwise           = (x,y) : symCoords (x+1) y
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+part1 :: [(Int,Int,Int)] -> [(Int,Int)] -> Int
+part1 pnums symbs = 1
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+part2 :: [(Int,Int,Int)]-> [(Int,Int)] -> Int
+part2 pnums symbs = 2
