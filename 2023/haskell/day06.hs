1bf8f7851aa1001ec4508b0d682e460a25561e01
venanzio <venanzio@duplavis.com>
2023-12-06 06:34:25 +0000

    day 6 done

--- 2023/haskell/day06.hs
+++ 2023/haskell/day06.hs
@@ -21,23 +21,49 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
-  putStrLn ("Part 1: " ++ show (part1 xs))
-  putStrLn ("Part 2: " ++ show (part2 xs))
+  putStrLn ("Part 1: " ++ show (part1 (input1 xs)))
+  putStrLn ("Part 2: " ++ show (part2 (input2 xs)))
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+pInput :: Parser ([String],[String])
+pInput = do symbol "Time:"
+            ts <- pLine (many word)
+            symbol "Distance:"
+            ds <- pLine (many word)
+            return (ts,ds)
 
-pInput :: Parser [()]
-pInput = pLines pData
+input1 :: ([String],[String]) -> [(Int,Int)]
+input1 (ts,ds) = zip (map read ts) (map read ds)
+
+input2 :: ([String],[String]) -> (Int,Int)
+input2 (ts,ds) = (read (concat ts), read (concat ds))
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+nextUp :: Double -> Int
+nextUp x = let x1 = floor x
+               x2 = ceiling x
+           in if x1==x2 then x1+1 else x2
+
+nextDown :: Double -> Int
+nextDown x = let x1 = floor x
+                 x2 = ceiling x
+             in if x1==x2 then x1-1 else x1
+               
+race :: (Int,Int) -> Int
+race (time,distance) =
+  let t = fromIntegral time
+      d = fromIntegral distance
+      disc = sqrt (t^2-4*d)
+      x1 = nextUp ((t - disc)/2)
+      x2 = nextDown ((t + disc)/2)
+  in x2 - x1 + 1
+
+part1 :: [(Int,Int)] -> Int
+part1 = product . map race 
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+part2 :: (Int,Int) -> Int
+part2 = race
