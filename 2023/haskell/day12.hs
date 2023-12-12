92a2c3a22dcfb8c39e298ba5a4cc4067cad8293c
venanzio <venanzio@duplavis.com>
2023-12-12 06:54:35 +0000

    2023 day 12 done - but Part 2 too slow

--- 2023/haskell/day12.hs
+++ 2023/haskell/day12.hs
@@ -27,18 +27,39 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+pData :: Parser (String,[Int])
+pData = do row <- token label
+           groups <- someSep natural (char ',')
+           return (row,groups)
 
-pInput :: Parser [()]
+pInput :: Parser [(String,[Int])]
 pInput = pLines pData
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+arrangements :: String -> [Int] -> Int
+arrangements r gs = sum
+  [arrangements0 (drop n r) gs |
+   n <- [0 .. length (takeWhile (`elem` ".?") r)] ]
+
+arrangements0 :: String -> [Int] -> Int
+arrangements0 r [] = if all (`elem` ".?") r then 1 else 0
+arrangements0 r [0] = if all (`elem` ".?") r then 1 else 0
+arrangements0 r gs | length r < sum gs + length gs - 1 = 0
+arrangements0 r (0:gs) = sum
+  [arrangements0 (drop n r) gs |
+   n <- [1 .. length (takeWhile (`elem` ".?") r)] ]
+arrangements0 r (g:gs) = if length (takeWhile (`elem` "#?") r) >= g
+  then arrangements0 (drop g r) (0:gs)
+  else 0
+
+part1 :: [(String,[Int])] -> Int
+part1 xs = sum [arrangements r gs | (r,gs) <- xs]
 
 -- Part 2
 
-part2 :: [()] -> Int
-part2 _ = 2
+unfold :: (String,[Int]) ->  (String,[Int])
+unfold (r,gs) = (concat (r:take 4 (repeat ('?':r))), concat (take 5 (repeat gs)))
+
+part2 :: [(String,[Int])] -> Int
+part2  xs = sum [arrangements r gs | (r,gs) <- map unfold xs]
