b689df891770e469888ed2636ba6f34bd4ac55e5
venanzio <venanzio@duplavis.com>
2023-12-14 14:57:00 +0000

    types of solution functions

---
+++ 2023/haskell/day14.hs
@@ -0,0 +1,42 @@
+-- Advent of Code 2023, day 14
+--  Venanzio Capretta
+
+module Main where
+
+import System.Environment
+import Data.List
+import Data.Char
+import Control.Applicative
+import qualified Data.Map as M
+
+import FunParser
+import AoCTools
+
+main :: IO ()
+main = do
+  args <- getArgs
+  puzzle (head args)
+
+puzzle :: String -> IO ()
+puzzle fileName = do
+  input <- readFile fileName >>= return . lines
+  let (round,cube) = pInput input
+  putStrLn ("Part 1: " ++ show (part1 round cube))
+  putStrLn ("Part 2: " ++ show (part2 round cube))
+
+-- Parsing the input
+
+type Position = (Int,Int)
+
+pInput :: [String] -> ([Position],[Position])
+pInput = undefined
+
+-- Part 1
+
+part1 :: [Position] -> [Position] -> Int
+part1 _ _ = 1
+
+-- Part 2
+
+part2 :: [Position] -> [Position] -> Int
+part2 _ _ = 2
