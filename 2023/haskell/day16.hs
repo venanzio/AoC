c9f425350520d63214a343eb36433cc93f876895
venanzio <venanzio@duplavis.com>
2023-12-16 15:46:32 +0000

    2023 day 16 skeleton file

---
+++ 2023/haskell/day16.hs
@@ -0,0 +1,43 @@
+-- Advent of Code 2023, day 16
+--  Venanzio Capretta
+
+module Main where
+
+import System.Environment
+import Data.List
+import Data.Char
+import Control.Applicative
+import qualified Data.Map as M
+
+import FunParser
+import AoCTools
+
+main :: IO ()
+main = do
+  args <- getArgs
+  puzzle (head args)
+
+puzzle :: String -> IO ()
+puzzle fileName = do
+  input <- readFile fileName
+  let xs = parseAll pInput input
+  putStrLn ("Part 1: " ++ show (part1 xs))
+  putStrLn ("Part 2: " ++ show (part2 xs))
+
+-- Parsing the input
+
+pData :: Parser ()
+pData = return ()
+
+pInput :: Parser [()]
+pInput = pLines pData
+
+-- Part 1
+
+part1 :: [()] -> Int
+part1 _ = 1
+
+-- Part 2
+
+part2 :: [()] -> Int
+part2 _ = 2
