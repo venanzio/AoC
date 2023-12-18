bacca4e96c7796e97ae38026ab53a34147378f9f
venanzio <venanzio@duplavis.com>
2023-12-18 10:43:36 +0000

    day 18 done (needs improving in Part 1)

--- 2023/haskell/day18.hs
+++ 2023/haskell/day18.hs
@@ -9,6 +9,8 @@ import Data.Char
 import Control.Applicative
 import qualified Data.Map as M
 
+import Numeric (readHex)
+
 import FunParser
 import AoCTools
 
@@ -22,18 +24,33 @@ puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
       t = trench (map (\(d,m,_)->(d,m)) xs)
+      t2 = rgbTrench (map (\(_,_,rgb) -> rgb) xs)
       a = area t
-  --putStrLn (show (head t) ++ show (last t))
+  putStrLn (show t2)
   -- putStrLn (showTrench t [])
-  putStrLn ("Part 1: " ++ show (part1 xs))
-  putStrLn ("Part 2: " ++ show (part2 xs))
+  --putStrLn ("Part 1: " ++ show (part1 xs))
+  putStrLn ("Part 2: " ++ show (part2 t2))
 
 -- Parsing the input
 
-type RGB = String
+type RGB = (Char,Int)
+
+hex :: String -> Int
+hex s = case readHex s of
+  [(x,"")] -> x
+  _ -> error "not an hex"
+  
 
 readRGB :: String -> RGB
-readRGB = id
+readRGB s =
+  ( case (hex [last s]) of
+      0 -> 'R'
+      1 -> 'D'
+      2 -> 'L'
+      3 -> 'U'
+      _ -> error "invalid direction"
+  , hex (take 5 s)
+  )
 
 pData :: Parser (Char,Int,RGB)
 pData = do direction <- item
@@ -81,5 +98,30 @@ part1 = length . nub . area . trench . map (\(d,m,_)->(d,m))
 
 -- Part 2
 
-part2 ::  [(Char,Int,RGB)] -> Int
-part2 _ = 2
+rgbTrench :: [RGB] -> [Point]
+rgbTrench = rgbt (0,0) where
+  rgbt p [] = [p]
+  rgbt (x,y) (('U',n):cis) = (x,y) : rgbt (x,y-n) cis
+  rgbt (x,y) (('D',n):cis) = (x,y) : rgbt (x,y+n) cis
+  rgbt (x,y) (('L',n):cis) = (x,y) : rgbt (x-n,y) cis
+  rgbt (x,y) (('R',n):cis) = (x,y) : rgbt (x+n,y) cis
+
+trenchLength :: [Point] -> Int
+trenchLength [] = 0
+trenchLength [p] = 0
+trenchLength ((x0,y0):(x1,y1):ps) = trenchLength ((x1,y1):ps) +
+  if x0==x1 then abs (y1-y0) else abs (x1-x0)
+
+shoelace :: [Point] -> Int
+shoelace ps = abs (sum [rectangle p0 p1 | (p0,p1) <- zip ps (tail ps)])
+              + trenchLength ps `div` 2 +1
+
+
+
+rectangle :: Point -> Point -> Int
+rectangle (x0,y0) (x1,y1) =
+  if y0==y1 then (x1-x0)*y0
+            else 0
+
+part2 ::  [Point] -> Int
+part2 = shoelace 
