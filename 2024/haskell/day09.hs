0a612afdd6f31d0b58b50c5339ed4972be350e08
venanzio <venanzio@duplavis.xyz>
2025-07-28 22:33:50 +0100

    day 9 clean-up

--- 2024/haskell/day09.hs
+++ 2024/haskell/day09.hs
@@ -18,39 +18,29 @@ main = do
 puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
-  let xs = parseAll pInput input
-  putStrLn ("Part 1: " ++ show (part1 xs))
-  putStrLn ("Part 2: " ++ show (part2 xs))
+  let bs = diskBlocks $ parseAll pInput input
+  putStrLn ("Part 1: " ++ show (part1 bs))
+  putStrLn ("Part 2: " ++ show (part2 bs))
 
 -- Parsing the input
 
 pInput :: Parser [Int]
 pInput = some (digit >>= \d -> return (read [d]))
 
+dBlocks :: Int -> [Int] -> [(Int,Int,Int)]
+dBlocks n (bl:sp:dmap) = (bl,n,sp) : dBlocks (n+1) dmap
+dBlocks n [bl] = [(bl,n,0)]
+dBlocks n [] = []
+
+diskBlocks :: [Int] -> [(Int,Int,Int)]
+diskBlocks = dBlocks 0
+
 -- Part 1
 
-diskBlocks :: [Int] -> [Int]
-diskBlocks = dBlocks 0 where
-  dBlocks n (f:e:map) = take f (repeat n) ++ take e (repeat (-1)) ++ dBlocks (n+1) map
-  dBlocks n [f] = take f (repeat n)
-  dBlocks _ [] = []
-
-
-compact :: [Int] -> [Int]
-compact = auxCompact . listLW where
-  auxCompact blocks
-    | isEmptyW blocks = []
-    | h  >= 0 = h : auxCompact blocks1
-    | isEmptyW blocks1 = []
-    | l <0 = auxCompact (insertW (-1) blocks2)
-    | otherwise = l : auxCompact blocks2
-    where (h,blocks1) = extractW blocks
-          (l,blocks2) = extractW (leftW blocks1)
-
-checkSum :: [Int] -> Int
-checkSum = sum . zipWith (*) [0..]
-
---
+compact1 :: [(Int,Int,Int)] -> [(Int,Int,Int)]
+compact1 bs = case unsnoc bs of
+  Nothing -> []
+  Just (bs0,(size,f,sp)) -> bInsert1 size f bs0
 
 bInsert1 :: Int -> Int -> [(Int,Int,Int)] -> [(Int,Int,Int)]
 bInsert1 size f [] = [(size,f,0)]
@@ -60,25 +50,16 @@ bInsert1 size f ((s0,f0,sp):bs)
   | sp == size = (s0,f0,0) : (size,f,0) : compact1 bs
   | sp > size = (s0,f0,0) : compact1 ((size,f,sp-size):bs)
 
-compact1 :: [(Int,Int,Int)] -> [(Int,Int,Int)]
-compact1 bs = case unsnoc bs of
-  Nothing -> []
-  Just (bs0,(size,f,sp)) -> bInsert1 size f bs0
+checkSum :: [(Int,Int,Int)] -> Int
+checkSum = checkSumAux 0 where
+  checkSumAux _ [] = 0
+  checkSumAux n ((size,f,sp):bs) =
+    sum (map (*f) [n..n+size-1]) + checkSumAux (n+size+sp) bs
 
+part1 :: [(Int,Int,Int)] -> Int
+part1 = checkSum . compact1
 
-
-part1 :: [Int] -> Int
--- part1 = checkSum . compact . diskBlocks
-part1 = checkSum2 . compact1 . diskBlocks2
 -- Part 2
-
-dBlocks :: Int -> [Int] -> [(Int,Int,Int)]
-dBlocks n (bl:sp:dmap) = (bl,n,sp) : dBlocks (n+1) dmap
-dBlocks n [bl] = [(bl,n,0)]
-dBlocks n [] = []
-
-diskBlocks2 :: [Int] -> [(Int,Int,Int)]
-diskBlocks2 = dBlocks 0
 
 bSpace :: (Int,Int,Int) -> Int
 bSpace (_,_,sp) = sp
@@ -99,11 +80,5 @@ compact2 bs = case unsnoc bs of
       Just bs1 -> let Just (bs2,(size2,f2,sp2)) = unsnoc bs1
                   in compact2 (bs2 ++ [(size2,f2,sp2+size+sp)])
 
-checkSum2 :: [(Int,Int,Int)] -> Int
-checkSum2 = checkSumAux 0 where
-  checkSumAux _ [] = 0
-  checkSumAux n ((size,f,sp):bs) =
-    sum (map (*f) [n..n+size-1]) + checkSumAux (n+size+sp) bs
-
-part2 :: [Int] -> Int
-part2 = checkSum2 . compact2 . diskBlocks2
+part2 :: [(Int,Int,Int)] -> Int
+part2 = checkSum . compact2
