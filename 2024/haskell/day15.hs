5f83174e8bed0344978eef81fcd8cf313209926d
venanzio <venanzio@duplavis.xyz>
2025-08-02 11:46:57 +0100

    executing all the robot moves

--- 2024/haskell/day15.hs
+++ 2024/haskell/day15.hs
@@ -22,8 +22,8 @@ puzzle fileName = do
   input <- readFile fileName
   let (h,ms) = parseAll pInput input
   putStrLn (showMap id h)
-  putStrLn (show $ robot h)
-  putStrLn ms
+  putStrLn (showMap id $ movesR (robot h) ms h)
+  -- stepMoves (robot h) ms h
   putStrLn ("Part 1: " ++ show (part1 h ms))
   putStrLn ("Part 2: " ++ show (part2 h ms))
 
@@ -56,10 +56,39 @@ mDir 'v' = dDown
 mDir '<' = dLeft
 mDir '>' = dRight
 
-move :: Point -> Direction -> Map2D Char -> Map2D Char
-move p d h = let p0 = pMove p d in
+moveH :: Point -> Direction -> Map2D Char -> Maybe (Map2D Char)
+moveH p d h = let p0 = pMove p d in
   case M.lookup p0 h of
-    Nothing -> undefined
+    Nothing -> Just $ mMove p p0 h
+    Just '#' -> Nothing
+    Just 'O' -> case moveH p0 d h of
+                  Nothing -> Nothing
+                  Just h0 -> Just $ mMove p p0 h0
+    _ -> error ("unexpected character at "++show p0)
+
+moveR :: Point -> Direction -> Map2D Char -> (Point,Map2D Char)
+moveR p d h = case moveH p d h of
+  Nothing -> (p,h)
+  Just h0 -> (pMove p d,h0)
+
+stepMoves :: Point -> String -> Map2D Char -> IO ()
+stepMoves p [] h = putStrLn (showMap id h)
+stepMoves p (m:ms) h  = do
+    putStrLn (showMap id h)
+    putStrLn (show p)
+    let d = mDir m
+        (p0,h0) = moveR p d h
+    stepMoves p0 ms h0
+
+movesR :: Point -> String -> Map2D Char -> Map2D Char
+movesR p [] h = h
+movesR p (m:ms) h = let d = mDir m
+                        (p0,h0) = moveR p d h
+                    in movesR p0 ms h0
+
+
+
+
 
 part1 :: Map2D Char -> String -> Int
 part1 h ms = 1
