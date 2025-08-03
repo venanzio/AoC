82acbceacb6a98a48ef24b9a6214311492f1b074
venanzio <venanzio@duplavis.xyz>
2025-08-03 16:28:05 +0100

    part 1 done

--- 2024/haskell/day16.hs
+++ 2024/haskell/day16.hs
@@ -4,7 +4,7 @@
 module Main where
 
 import System.Environment
--- import Data.List
+import Data.List
 -- import Data.Char
 -- import Control.Applicative
 import qualified Data.Map as M
@@ -21,7 +21,7 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let maze = stringsMap (lines input)
-  putStrLn (showMap id maze)
+  putStrLn (show (mazeGraph maze))
   putStrLn ("Part 1: " ++ show (part1 maze))
   putStrLn ("Part 2: " ++ show (part2 maze))
 
@@ -29,25 +29,24 @@ puzzle fileName = do
 
 mazeGraph :: Map2D Char -> Graph (Point,Direction)
 mazeGraph maze = M.fromList $ [(n,step n) | i <- [1..maxX], j <- [1..maxY],
-                                            n <- node (i,j)] ++
-                              [((pE,d),[((pE,(0,0)),0)]) | d <- directionsHV] ++
-                              [((pE,(0,0)),[])]
+                                            n <- node (i,j)]
   where (maxX,maxY) = fst $ M.findMax maze
         pE = head $ mFind 'E' maze
-        node p = case M.lookup p maze of
+        node p = (pE,(0,0)) : case M.lookup p maze of
           Just '#' -> []
           _ -> map (\d -> (p,d)) directionsHV
-        step (p,d)   = if M.lookup p maze /= Just '#'
-                         then [((pMove p d,d),1)]
-                         else []
-                       ++ [((p,dRTurn d),1000),
-                           ((p,dLTurn d),1000)]
+        step (p,d)
+          | p == pE && d == (0,0) = []
+          | p == pE               = [((pE,(0,0)),0)]
+          | M.lookup (pMove p d) maze /= Just '#' = ((pMove p d,d),1) : turns
+          | otherwise = turns
+          where turns = [((p,dRTurn d),1000),((p,dLTurn d),1000)]
 
   
 part1 :: Map2D Char -> Int
 part1 maze = dijkstra (mazeGraph maze)
                       (head $ mFind 'S' maze, dRight)
-                      (head $ mFind 'S' maze, (0,0))
+                      (head $ mFind 'E' maze, (0,0))
 
 -- Part 2
 
