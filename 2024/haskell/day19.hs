d79654dfd0ff4e8e053835dfede68339aaa71b27
venanzio <venanzio@duplavis.xyz>
2025-08-09 22:28:38 +0100

    dynamic programming works for part1, deleted old attempts

--- 2024/haskell/day19.hs
+++ 2024/haskell/day19.hs
@@ -22,8 +22,6 @@ puzzle fileName = do
   input <- readFile fileName
   let (patterns',designs) = parseAll pInput input
       patterns = reverse $ sortOn length patterns'
-  putStrLn (show patterns)
-  putStrLn (show designs)
   putStrLn ("Part 1: " ++ show (part1 patterns designs))
   putStrLn ("Part 2: " ++ show (part2 patterns designs))
 
@@ -42,35 +40,9 @@ pInput = do patterns <- pPatterns
             
 -- Part 1
 
-mkDes :: [String] -> String -> Bool
-mkDes patterns [] = True
-mkDes patterns design = any (mkDes patterns)
-  [pTail | Just pTail <- map (\p -> stripPrefix p design) patterns]
-
---
-
-correctDes :: [String] -> String -> Bool
-correctDes pats [] = True
-correctDes []   des = False
-correctDes (pat:pats) des = any splitCorrect (cutInfix pat des) ||
-                            correctDes pats des
-    where splitCorrect (pre,post) = correctDes pats pre &&
-                                    correctDes (pat:pats) post
-  
-cutInfix :: String -> String -> [(String,String)]
-cutInfix pat des =
-  [prepost | Just prepost <- map (\i -> cutInfixAt i pat des) [0..length des]]
-
-cutInfixAt :: Int -> String -> String -> Maybe (String,String)
-cutInfixAt i pat des = let (pre,rest) = splitAt i des
-                       in stripPrefix pat rest >>= \post -> return (pre,post)
---
-
-sublists :: [a] -> [[a]]
 sublists = concat. map inits1 . tails
   where inits1 [] = [[]]
         inits1 (x:xs) = map (x:) (inits xs)
-
 
 correct :: [String] -> String -> Bool
 correct pats des = correctTable M.! des
