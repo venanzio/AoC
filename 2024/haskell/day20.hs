daf933bca9568bd520a7fcdd2a89b7a1bca3fd3d
venanzio <venanzio@duplavis.xyz>
2025-08-10 19:17:32 +0100

    idea: use the complete graph and break path in two for cheats

--- 2024/haskell/day20.hs
+++ 2024/haskell/day20.hs
@@ -21,8 +21,11 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let mp = stringsMap $ lines input
+      wall = M.keys $ M.filter (=='#') mp
+      gr = mazeGraph wall
       start = head $ mFind 'S' mp
       end = head $ mFind 'E' mp
+      
   putStrLn ("Part 1: " ++ show (part1 mp))
   putStrLn ("Part 2: " ++ show (part2 mp))
 
@@ -36,6 +39,7 @@ pInput = pLines pData
 
 -- Part 1
 
+{-
 -- Extended graph: each point has an int indicating the cheats
 --  0 = no cheat, 1 = first step of the cheat, 2 = used the cheat
 graph :: Map2D Char -> Graph (Point,Int)
@@ -49,9 +53,15 @@ graph mp = M.fromList [((p,i), map (\q->(q,1)) (step (p,i))) |
           1 -> map (\q->(q,2)) (neighboursHV p) 
           2 -> map (\q->(q,2)) fs
           where (fs,ws) = partition free (neighboursHV p) 
+-}
+
+mdGraph :: Map2D Char -> Graph Point
+mdGraph md = M.fromList [(p,step p) | p <- allPoints pMin pMax]
+  where (pMin,pMax) = minMaxPoints (M.keys md)
+        step p = undefined
 
 part1 :: Map2D Char -> Int
-part1 _ = 1
+part1 md = undefined
 
 -- Part 2
 
