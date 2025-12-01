6a91b850b0869e55f38d2d55b730a433baad463a
venanzio <venanzio@duplavis.xyz>
2025-12-01 09:34:24 +0000

    siplified and generalized

--- 2025/haskell/day01.hs
+++ 2025/haskell/day01.hs
@@ -19,7 +19,6 @@ puzzle fileName = do
   let xs = parseAll pInput input
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
-  -- putStrLn (show (rot2 xs))
 
 -- Parsing the input
 
@@ -32,38 +31,28 @@ pInput = pLines pData
 
 -- Part 1
 
-rotate :: [Int] -> Int
-rotate = rotateAux 50 where
-  rotateAux x [] = 0
-  rotateAux x (y:ys) =
-    let x' = (x+y) `mod` 100
-        inc = if x' == 0 then 1 else 0
-    in inc + rotateAux x' ys
+rot1 :: Int -> Int -> (Int,Int)
+rot1 x y = let x' = (x+y) `mod` 100
+               inc = if x' == 0 then 1 else 0
+           in (inc, x')
 
+rotate :: (Int -> Int -> (Int,Int) ) -> [Int] -> Int
+rotate rot = rotateAux 50 where
+  rotateAux x [] = 0
+  rotateAux x (y:ys) = let (inc,x') = rot x y
+                       in inc + rotateAux x' ys
 
 part1 :: [Int] -> Int
-part1 = rotate
+part1 = rotate rot1
 
 -- Part 2
 
-rotate2 :: [Int] -> Int
-rotate2 = rotateAux 50 where
-  rotateAux x [] = 0
-  rotateAux x (y:ys) = let xRaw = x+y
-                           inc = if y > 0 then xRaw `div` 100
-                                 else (((100-x) `mod` 100) - y) `div` 100
-                           x' = xRaw `mod` 100
-                       in inc + rotateAux x' ys
-
-rot2 :: [Int] -> [(Int,Int)]
-rot2 = rotateAux 50 where
-  rotateAux x [] = []
-  rotateAux x (y:ys) = let xRaw = x+y
-                           inc = if y > 0 then xRaw `div` 100
-                                 else ((100-x) - y) `div` 100
-                           x' = xRaw `mod` 100
-                       in (inc,x') : rotateAux x' ys
+rot2 :: Int -> Int -> (Int,Int)
+rot2 x y = (inc, x' `mod` 100) where
+  x' = x+y
+  x'' = if x==0 then -y else 100-x-y
+  inc = if y>=0 then x' `div` 100 else x'' `div` 100
 
 part2 :: [Int] -> Int
-part2 = rotate2
+part2 = rotate rot2
 
