0bc31ef2c421ad7b62ca6f53fa58d9efd3fef7bd
venanzio <venanzio@duplavis.xyz>
2025-12-02 06:55:54 +0000

    day 2 part 1

--- 2025/haskell/day02.hs
+++ 2025/haskell/day02.hs
@@ -26,18 +26,75 @@ puzzle fileName = do
 
 -- Parsing the input
 
-pData :: Parser ()
-pData = return ()
+pData :: Parser (Int,Int)
+pData = do x <- natural
+           symbol "-"
+           y <- natural
+           return (x,y)
 
-pInput :: Parser [()]
-pInput = pLines pData
+pInput :: Parser [(Int,Int)]
+pInput = someSepStr pData ","
 
 -- Part 1
 
-part1 :: [()] -> Int
-part1 _ = 1
+log10 :: Int -> Int
+log10 0 = 0
+log10 x = 1 + log10 (x `div` 10)
+
+splitUp :: Int -> (Int,Int)
+splitUp x = if even n then (x1,x2) else (x',x') where
+  n = log10 x
+  x' = 10^(n `div` 2)
+  x1 = x `div` x'
+  x2 = max (x - x1 * x') (x' `div` 10)
+
+splitDown :: Int -> (Int,Int)
+splitDown x = if even n then (x1,x2) else (x'-1,x'-1) where
+  n = log10 x
+  x' = 10^(n `div` 2)
+  x1 = x `div` x'
+  x2 = x - x1 * x'
+
+invUp :: Int -> Int
+invUp x | odd n = x'
+        | x1 >= x2 = x1
+        | otherwise = x1+1
+  where n = log10 x
+        x' = 10^(n `div` 2)
+        x1 = x `div` x'
+        x2 = x - x1 * x'
+
+invDown :: Int -> Int
+invDown x | odd n = x'-1
+          | x1 <= x2 = x1
+          | otherwise = x1-1
+  where n = log10 x
+        x' = 10^(n `div` 2)
+        x1 = x `div` x'
+        x2 = x - x1 * x'
+
+dupl :: Int -> Int
+dupl x = x * (1 + 10^(log10 x))
+
+invalid :: Int -> Int -> [Int]
+invalid x y = map dupl [invUp x .. invDown y] where
+  (x1,x2) = splitUp x
+  (y1,y2) = splitDown y
+  
+
+part1 :: [(Int,Int)] -> Int
+part1 xys = sum $ concat [invalid x y | (x,y) <- xys]
 
 -- Part 2
 
-part2 :: [()] -> Int
+res :: Int -> Int -> Int
+res x m = x - (x `div` 10^m) * 10^m
+
+repDigs :: Int -> Int -> Bool
+repDigs x m = isRepeat x where
+  res = x - (x `div` 10^m) * 10^m
+  isRepeat 0 = True
+  isRepeat y = False
+
+part2 :: [(Int,Int)] -> Int
 part2 _ = 2
