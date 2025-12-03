2b7f6e6a3192a1a85ad221a5e2844025a0b68159
venanzio <venanzio@duplavis.xyz>
2025-12-03 22:41:58 +0000

    day 3 cleaned and generalized

--- 2025/haskell/day03.hs
+++ 2025/haskell/day03.hs
@@ -4,13 +4,10 @@
 module Main where
 
 import System.Environment
-import Data.List
-import Data.Char
 import Control.Applicative
-import qualified Data.Map as M
 
-import FunParser_old
-import AoCTools_old
+import FunParser
+import AoCTools
 
 main :: IO ()
 main = do
@@ -21,7 +18,6 @@ puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
   let xs = parseAll pInput input
-  putStrLn (show xs)
   putStrLn ("Part 1: " ++ show (part1 xs))
   putStrLn ("Part 2: " ++ show (part2 xs))
 
@@ -33,25 +29,17 @@ pData = some (digit >>= \d -> return $ read [d])
 pInput :: Parser [[Int]]
 pInput = pLines pData
 
--- Part 1
-
-joltage :: [Int] -> Int
-joltage b = x * 10 + maximum (tail bt) where
-  x = maximum (init b)
-  (_,bt) = break (== x) b
-
-part1 :: [[Int]] -> Int
-part1 = sum . map joltage
-
--- Part 2
+-- Solutions
 
 jolt :: Int -> [Int] -> Int
 jolt 0 _ = 0
-jolt n b = x*10^(n-1) + jolt (n-1) (tail bt) where
-  bi = take (length b - n + 1) b
+jolt k b = x*10^(k-1) + jolt (k-1) (tail bt) where
+  bi = take (length b - k + 1) b
   x = maximum bi
   (_,bt) = break (==x) b
 
+part1 :: [[Int]] -> Int
+part1 = sum . map (jolt 2)
 
 part2 :: [[Int]] -> Int
 part2 = sum . map (jolt 12)
