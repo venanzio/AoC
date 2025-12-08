b5ae2fbca3407019b99cf01864b6d158c4c928a5
venanzio <venanzio@duplavis.xyz>
2025-12-08 10:40:56 +0000

    cleaned up and rearranged

--- 2025/haskell/day08.hs
+++ 2025/haskell/day08.hs
@@ -5,7 +5,6 @@ module Main where
 
 import System.Environment
 import Data.List
-import Data.Char
 import Control.Applicative
 import qualified Data.Map as M
 
@@ -47,63 +46,22 @@ pInput = some pData
 dist :: Point3D -> Point3D -> Int
 dist (x0,y0,z0) (x1,y1,z1) = (x0-x1)^2 + (y0-y1)^2 + (z0-z1)^2
 
-{-
-closest :: Point3D -> [Point3D] -> (Point3D,Int)
-closest p [] = ((1000,1000,1000),1000000000)
-closest p (p0:ps) = let d0 = dist p p0
-                        (p1,d1) = closest p ps
-                    in if d0 < d1 then (p0,d0) else (p1,d1)
-                                                    
-closePair ::  [Point3D] -> Circuit -> (Point3D,Point3D)
-closePair ps circuits = (\(p0,(p1,_)) -> (p0,p1)) $
-  minimumBy (\(_,(_,d0)) (_,(_,d1)) -> compare d0 d1) $
-  map (\p -> (p, closest p (ps\\M.findWithDefault [] p circuits))) ps
--}
+connections :: [Point3D] -> [(Point3D,Point3D)]
+connections ps = sortOn (\(p0,p1) -> dist p0 p1) (allPairs ps)
 
 type Circuit = M.Map Point3D [Point3D]
 
 noConnect :: [Point3D] -> Circuit
 noConnect = M.fromList . map (\p -> (p,[p]))
 
-{-
 connect :: Point3D -> Point3D -> Circuit -> Circuit
-connect p0 p1 circuits =
-  M.update (\c -> Just (union c [p1])) p0 $
-    M.update (\c -> Just (union c [p0])) p1 circuits
-
-connectClose :: [Point3D] -> Circuit -> Circuit
-connectClose ps circuits = connect p0 p1 circuits where
-  (p0,p1) = closePair ps circuits
-
-connectAll :: Int -> [Point3D] -> Circuit
-connectAll 0 ps = noConnect ps
-connectAll n ps = connectClose ps (connectAll (n-1) ps)
-
-largestCircuit :: Circuit -> [Point3D]
-largestCircuit = maximumBy (\c0 c1 -> compare (length c0) (length c1)) . M.elems
--}
+connect p0 p1 circuits = foldr (\p -> M.insert p circuit) circuits circuit
+  where circuit = union (M.findWithDefault [] p0 circuits)
+                        (M.findWithDefault [] p1 circuits)
+                  
 deleteCircuit :: [Point3D] -> Circuit -> Circuit
 deleteCircuit [] circuits = circuits
 deleteCircuit (c:cs) circuits = deleteCircuit cs (M.delete c circuits)
-{-
-fullCircuit :: [Point3D] -> Circuit -> ([Point3D], Circuit)
-fullCircuit [] circuits = ([],circuits)
-fullCircuit (p:ps) circuits =
-  (nub (p:connected++circuit), circuits2) where
-    connected = M.findWithDefault [] p circuits
-    circuits1 = M.delete p circuits
-    (circuit,circuits2) = fullCircuit (nub $ connected++ps) circuits1
-
-allCircuits :: [Point3D] -> Circuit -> [[Point3D]]
-allCircuits [] _ = []
-allCircuits (p:ps) circuits = circuit : allCircuits (ps\\circuit) circuits' where
-  (circuit, circuits') = fullCircuit [p] circuits
-  
-part1 :: [Point3D] -> Int
-part1 ps = length (cs!!0) * length (cs!!1) * length (cs!!2) where
-  circuits = connectAll 1000 ps
-  cs = sortBy (\l1 l2 -> compare (length l2) (length l1)) $ allCircuits ps circuits
--}
 
 connectAll :: [(Point3D,Point3D)] -> Circuit -> Circuit
 connectAll ps circuits = foldl (\c (p0,p1) -> connect p0 p1 c) circuits ps
@@ -117,17 +75,9 @@ allCircuits (p:ps) circuits = circuit : allCircuits (ps\\circuit) circuits' wher
 part1 :: [Point3D] -> Int
 part1 ps = product (take 3 lengths) where
   circuits = connectAll (take 1000 (connections ps)) (noConnect ps)
-  lengths = sortBy (\l1 l2 -> compare l2 l1) $ map length $ allCircuits ps circuits
+  lengths = sortBy (flip compare) $ map length $ allCircuits ps circuits
 
 -- Part 2
-
-connections :: [Point3D] -> [(Point3D,Point3D)]
-connections ps = sortOn (\(p0,p1) -> dist p0 p1) (allPairs ps)
-
-connect :: Point3D -> Point3D -> Circuit -> Circuit
-connect p0 p1 circuits = foldr (\p -> M.insert p circuit) circuits circuit
-  where circuit = union (M.findWithDefault [] p0 circuits)
-                        (M.findWithDefault [] p1 circuits)
 
 oneCircuit :: [(Point3D,Point3D)] -> Circuit -> (Point3D,Point3D)
 oneCircuit [] circuits = (maxPoint,maxPoint)
