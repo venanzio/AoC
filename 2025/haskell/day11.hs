c92c2775c19270bdc3edfde223f2e9add9a4b339
venanzio <venanzio@duplavis.xyz>
2025-12-11 07:33:55 +0000

    part 2 solved

--- 2025/haskell/day11.hs
+++ 2025/haskell/day11.hs
@@ -20,27 +20,52 @@ main = do
 puzzle :: String -> IO ()
 puzzle fileName = do
   input <- readFile fileName
-  let xs = parseAll pInput input
-  putStrLn ("Part 1: " ++ show (part1 xs))
-  putStrLn ("Part 2: " ++ show (part2 xs))
+  let g = parseAll pInput input
+      ps = paths g
+  putStrLn ("Part 1: " ++ show (part1 ps))
+  putStrLn ("Part 2: " ++ show (part2 ps))
 
 -- Parsing the input
 
 pData :: Parser (String,[String])
-pData = do device <- label
-           symbol "'"
-           outputs <- some label
+pData = do device <- identifier
+           symbol ":"
+           outputs <- some identifier
            return (device,outputs)
 
-pInput :: Parser [(String,[String])]
-pInput = pLines pData
+pInput :: Parser (Graph String)
+pInput = do input <- pLines pData
+            return (M.fromList (("out",[]):[(d,map (\o -> (o,1)) os) | (d,os) <- input]))
 
 -- Part 1
 
-part1 :: [(String,[String])] -> Int
-part1 _ = 1
+paths :: Graph String -> M.Map (String,String) Int
+paths g = pathsM where
+  pathsM = M.fromList [((v0,v1), pathsAux v0 v1) | v0 <- devs, v1 <- devs]
+  pathsAux v0 v1
+    | v0==v1 = 1
+    | otherwise = sum $ [M.findWithDefault 0 (w,v1) pathsM
+    | (w,_) <- M.findWithDefault undefined v0 g]
+
+part1 :: M.Map (String,String) Int -> Int
+part1 ps = M.findWithDefault undefined ("you","out") ps
 
 -- Part 2
 
-part2 :: [(String,[String])] -> Int
-part2 _ = 2
+part2 :: Graph String -> Int
+part2 ps = M.findWithDefault 0 ("svr", "dac") ps *
+           M.findWithDefault 0 ("dac", "fft") ps *
+           M.findWithDefault 0 ("fft", "out") ps +
+           M.findWithDefault 0 ("svr", "fft") ps *
+           M.findWithDefault 0 ("fft", "dac") ps *
+           M.findWithDefault 0 ("dac", "out") ps
+{-           
+  where devs = M.keys g
+        pathsM :: M.Map (String,String) Int
+        pathsM = M.fromList [((v0,v1), pathsAux v0 v1) | v0 <- devs, v1 <- devs]
+
+        pathsAux v0 v1
+          | v0==v1 = 1
+          | otherwise = sum $ [M.findWithDefault 0 (w,v1) pathsM
+                              | (w,_) <- M.findWithDefault undefined v0 g]
+-}
